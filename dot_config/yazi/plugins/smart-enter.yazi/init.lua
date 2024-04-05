33946d5809e78075d6e8c1ad69092468364170a5
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-04-05 17:21:37 +0800

    update yazi

---
+++ dot_config/yazi/plugins/smart-enter.yazi/init.lua
@@ -0,0 +1,8 @@
+-- https://yazi-rs.github.io/docs/tips#smart-enter
+-- Smart enter: `enter` for directory, `open` for file
+return {
+	entry = function()
+		local h = cx.active.current.hovered
+		ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
+	end,
+}
