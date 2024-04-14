887fce5f4e5152ee400dc8e72cc6cb3e7e2e60ec
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-04-15 00:39:48 +0800

    daily update

---
+++ dot_config/yabai/executable_focus_last_window_id.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+# https://github.com/zegervdv/homebrew-zathura/issues/62#issuecomment-1413968157
+
+function last_window_id(){
+    local app=$1
+    id=$(yabai -m query --windows | jq -e "map(select(.app==\"$app\")) | .[0] | .id") && echo $id
+}
+
+function focus_last_window_id(){
+    local app=$1
+    id=$(last_window_id $app) && yabai -m window --focus $id
+}
+
+focus_last_window_id $@
