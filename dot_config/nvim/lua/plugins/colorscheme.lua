1d5c870fad9cd9fd42fd5a29d5e8387b28e48c87
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-06-17 12:34:40 +0800

    Update LazyVim

---
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,39 @@
+return {
+  -- https://github.com/folke/dot/blob/master/nvim/lua/plugins/colorscheme.lua
+  -- https://www.lazyvim.org/configuration/recipes#make-tokyonight-transparent
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     style = "moon",
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+
+  -- https://github.com/craftzdog/dotfiles-public/blob/master/.config/nvim/lua/plugins/colorscheme.lua
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+
+  -- { "ellisonleao/gruvbox.nvim" },
+
+  -- TODO https://github.com/craftzdog/dotfiles-public/blob/bf837d867b1aa153cbcb2e399413ec3bdcce112b/.config/nvim/lua/config/lazy.lua#L21
+  -- https://www.lazyvim.org/plugins/colorscheme
+  -- Configure LazyVim to load another colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
