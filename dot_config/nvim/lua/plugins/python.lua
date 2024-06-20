c1cf746ea67544528b6696def92e8ee109c9235b
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-06-20 15:36:53 +0800

    Update LazyVim: venv-selector support for miniconda3 and temporary fix for extras.editor.fzf

--- dot_config/nvim/lua/plugins/python.lua
+++ dot_config/nvim/lua/plugins/python.lua
@@ -1,26 +1,44 @@
 -- LazyVim Extras: lang.python
 -- ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/plugins/extras/lang/python.lua
 return {
-  -- makes pyright and debugpy aware of the selected virtual environment
-  -- https://github.com/chrisgrieser/nvim-kickstart-python/blob/ad0ef92a37618f76cf815800800c1f13c04dcf68/kickstart-python.lua#L424
-  -- https://github.com/Relrin/dotfiles/blob/7c8526d7f8ade76310bdd949d31929edddd086c8/nvim/lua/plugins/extras/lang/python.lua#L82
+  -- note that LazyVim use the new "regexp" branch: https://github.com/linux-cultist/venv-selector.nvim/tree/regexp
   {
     "linux-cultist/venv-selector.nvim",
-    opts = function(_, opts)
-      if LazyVim.has("nvim-dap-python") then
-        opts.dap_enabled = true -- makes the debugger work with venv
-      end
-      return vim.tbl_deep_extend("force", opts, {
-        -- TODO can't detect miniconda3 env
-        -- anaconda_base_path = "~/miniconda3",
-        -- anaconda_envs_path = "~/miniconda3/envs",
-        name = {
-          "venv",
-          ".venv",
-          "env",
-          ".env",
+    -- TODO temp fix: venv-selector does not work with extras.editor.fzf
+    -- https://github.com/LazyVim/LazyVim/issues/3612
+    -- https://github.com/linux-cultist/venv-selector.nvim/issues/142
+    dependencies = {
+      { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+    },
+    -- https://github.com/linux-cultist/venv-selector.nvim/tree/regexp?tab=readme-ov-file#your-own-anaconda-search
+    -- https://github.com/Spreadprism/nvim/blob/c0f60a5dac485651e7a8005155c42ea6fb8b3069/lua/plugins/lsp.lua#L27
+    -- https://github.com/popshia/nvim/blob/0b1567719e2b2cfed0e96d67c804e1379ced9a76/lua/user/plugins/venv-selector.lua#L22
+    opts = {
+      settings = {
+        options = {
+          -- -- missing duplicate name of different path
+          -- on_telescope_result_callback = function(filename)
+          --   filename = filename:gsub("/bin/python", "") ---@type string
+          --   filename = vim.split(filename, "/")
+          --   filename = filename[#filename]
+          --   if filename == "miniconda3" then
+          --     filename = "base"
+          --   end
+          --   return filename
+          -- end,
+          -- for linux/mac: replace the home directory with '~' and remove the /bin/python part.
+          on_telescope_result_callback = function(filename)
+            return filename:gsub(os.getenv("HOME"), "~"):gsub("/bin/python", "")
+          end,
         },
-      })
-    end,
+        search = {
+          anaconda_base = {
+            -- command = "fd /bin/python$ ~/miniconda3 --full-path --color never -E /proc -E /pkgs", -- include `base` of calling `conda env list`
+            command = "fd /bin/python$ ~/miniconda3/envs --full-path --color never -E /proc",
+            type = "anaconda",
+          },
+        },
+      },
+    },
   },
 }
