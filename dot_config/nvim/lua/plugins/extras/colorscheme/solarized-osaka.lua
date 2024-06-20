16aff2a87c24efcf7bf97e19eef84b209f25cda7
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-06-20 13:52:45 +0800

    Update LazyVim: try extras.editor.fzf, add user extras of colorscheme

---
+++ dot_config/nvim/lua/plugins/extras/colorscheme/solarized-osaka.lua
@@ -0,0 +1,26 @@
+-- https://github.com/craftzdog/dotfiles-public/blob/master/.config/nvim/lua/plugins/colorscheme.lua
+return {
+  -- disable tokyonight
+  { "folke/tokyonight.nvim", enabled = false },
+
+  -- add solarized-osaka
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = function()
+      return {
+        transparent = true,
+      }
+    end,
+  },
+
+  -- configure LazyVim to load solarized-osaka theme
+  -- https://github.com/craftzdog/dotfiles-public/blob/bf837d867b1aa153cbcb2e399413ec3bdcce112b/.config/nvim/lua/config/lazy.lua#L21
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "solarized-osaka",
+    },
+  },
+}
