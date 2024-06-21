baa142c85da34621ba5f34656496be353ed26013
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-06-21 12:31:39 +0800

    Update LazyVim: do not disable built-in themes in user extras of colorscheme

--- dot_config/nvim/lua/plugins/extras/colorscheme/onedarkpro.lua
+++ dot_config/nvim/lua/plugins/extras/colorscheme/onedarkpro.lua
@@ -1,8 +1,9 @@
 -- https://github.com/appelgriebsch/Nv/blob/e9a584090a69a8d691f5eb051e76016b65dfc0b7/lua/plugins/extras/ui/onedarkpro-theme.lua
 -- https://github.com/appelgriebsch/Nv/blob/e9a584090a69a8d691f5eb051e76016b65dfc0b7/lazyvim.json#L25
 return {
-  -- disable tokyonight
-  { "folke/tokyonight.nvim", enabled = false },
+  -- disable built-in themes
+  -- { "folke/tokyonight.nvim", enabled = false },
+  -- { "catppuccin/nvim", enabled = false },
 
   -- add onedarkpro
   {
