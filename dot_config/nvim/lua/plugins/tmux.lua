3054de2d382406ed004b35eede79128df2d456db
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-07-03 18:34:01 +0800

    LazyVim: add tmux.nvim

---
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -0,0 +1,22 @@
+return {
+  -- https://github.com/arturgoms/nvim/blob/045c55460e36e1d4163b426b2ac66bd710721ac5/lua/3thparty/plugins/tmux.lua
+  {
+    "aserowy/tmux.nvim",
+    event = "VeryLazy",
+    opts = {
+      copy_sync = {
+        -- sync all registers
+        enable = false,
+      },
+      -- define keybindings in keymaps.lua to override LazyVim's default keybindings
+      navigation = {
+        -- enables default keybindings (C-hjkl) for normal mode
+        enable_default_keybindings = false,
+      },
+      resize = {
+        -- enables default keybindings (A-hjkl) for normal mode
+        enable_default_keybindings = false,
+      },
+    },
+  },
+}
