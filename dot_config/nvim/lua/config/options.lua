5f2700ea506f88ad3f135a7e4fb87fdafbe1926c
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-07-07 12:48:41 +0800

    feat(lazyvim): make keymaps in keymaps.lua silent by default
    LazyVim: disable neovide's animations in zen-mode

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,10 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 local opt = vim.opt
 
-opt.relativenumber = false -- Relative line numbers
-
+opt.relativenumber = false
 -- https://github.com/folke/dot/blob/master/nvim/lua/config/options.lua
 opt.backup = true
 opt.backupdir = vim.fn.stdpath("state") .. "/backup"
