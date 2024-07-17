0b5091aa913566a7c59923d6e0c39ea2b8f017c0
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-07-17 12:02:47 +0800

    fix(lazyvim): only load tmux plugin if in tmux session

--- dot_config/nvim/lua/plugins/extras/util/tmux.lua
+++ dot_config/nvim/lua/plugins/extras/util/tmux.lua
@@ -1,12 +1,11 @@
-if vim.fn.executable("tmux") == 0 then
-  return {}
-end
-
 return {
   -- https://github.com/arturgoms/nvim/blob/045c55460e36e1d4163b426b2ac66bd710721ac5/lua/3thparty/plugins/tmux.lua
   {
     "aserowy/tmux.nvim",
     event = "VeryLazy",
+    enabled = function()
+      return vim.env.TMUX ~= nil
+    end,
     opts = {
       copy_sync = {
         -- sync all registers
