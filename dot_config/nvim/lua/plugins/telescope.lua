0a2c86adca83ccc4cbd9268b4e9323ad02ff3168
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-07-19 00:18:47 +0800

    feat(lazyvim): add undotree extras, fix telescope's lazy loading
    Fix telescope's lazy loading by remove `local telescope = require("telescope")`.

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -89,12 +89,12 @@ return {
         layout_strategy = "horizontal",
         layout_config = {
           horizontal = {
+            width = 0.8,
+            height = 0.8,
             prompt_position = "top",
+            preview_cutoff = 120,
             preview_width = 0.5,
           },
-          width = 0.8,
-          height = 0.8,
-          preview_cutoff = 120,
         },
         sorting_strategy = "ascending",
         winblend = 0,
