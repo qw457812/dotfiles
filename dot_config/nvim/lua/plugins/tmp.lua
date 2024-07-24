07b3dbbb6987e0a3dd6407bfb647e652f0af267c
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-07-24 15:38:17 +0800

    feat(lazyvim): add `leader-ua` for neovide
    Use `MeanderingProgrammer/markdown.nvim` instead of `OXY2DEV/markview.nvim`.

--- dot_config/nvim/lua/plugins/tmp.lua
+++ dot_config/nvim/lua/plugins/tmp.lua
@@ -22,77 +22,78 @@ return {
     },
   },
 
-  {
-    "OXY2DEV/markview.nvim",
-    enabled = true,
-    opts = {
-      checkboxes = { enable = false },
-      links = {
-        inline_links = {
-          hl = "@markup.link.label.markown_inline",
-          -- icon = " ",
-          icon = "󰌷 ",
-          icon_hl = "@markup.link",
-        },
-        images = {
-          hl = "@markup.link.label.markown_inline",
-          -- icon = " ",
-          icon = "󰥶 ",
-          icon_hl = "@markup.link",
-        },
-      },
-      code_blocks = {
-        style = "language",
-        hl = "CodeBlock",
-        pad_amount = 0,
-      },
-      list_items = {
-        shift_width = 2,
-        marker_minus = {
-          -- text = "●",
-          text = "",
-          hl = "@markup.list.markdown",
-        },
-        marker_plus = {
-          -- text = "●",
-          text = "",
-          hl = "@markup.list.markdown",
-        },
-        marker_star = {
-          -- text = "●",
-          text = "",
-          hl = "@markup.list.markdown",
-        },
-        marker_dot = {},
-      },
-      inline_codes = { enable = false },
-      headings = {
-        heading_1 = { style = "simple", hl = "Headline1" },
-        heading_2 = { style = "simple", hl = "Headline2" },
-        heading_3 = { style = "simple", hl = "Headline3" },
-        heading_4 = { style = "simple", hl = "Headline4" },
-        heading_5 = { style = "simple", hl = "Headline5" },
-        heading_6 = { style = "simple", hl = "Headline6" },
-      },
-      -- https://github.com/OXY2DEV/markview.nvim/issues/25#issuecomment-2224586784
-      options = {
-        on_enable = {
-          conceallevel = 2,
-          concealcursor = "",
-        },
-        on_disable = {
-          conceallevel = 0,
-          concealcursor = "",
-        },
-      },
-    },
-
-    ft = { "markdown", "norg", "rmd", "org" },
-    specs = {
-      "lukas-reineke/headlines.nvim",
-      enabled = false,
-    },
-  },
+  -- using MeanderingProgrammer/markdown.nvim now | https://github.com/LazyVim/LazyVim/commit/264abdf
+  -- {
+  --   "OXY2DEV/markview.nvim",
+  --   enabled = true,
+  --   opts = {
+  --     checkboxes = { enable = false },
+  --     links = {
+  --       inline_links = {
+  --         hl = "@markup.link.label.markown_inline",
+  --         -- icon = " ",
+  --         icon = "󰌷 ",
+  --         icon_hl = "@markup.link",
+  --       },
+  --       images = {
+  --         hl = "@markup.link.label.markown_inline",
+  --         -- icon = " ",
+  --         icon = "󰥶 ",
+  --         icon_hl = "@markup.link",
+  --       },
+  --     },
+  --     code_blocks = {
+  --       style = "language",
+  --       hl = "CodeBlock",
+  --       pad_amount = 0,
+  --     },
+  --     list_items = {
+  --       shift_width = 2,
+  --       marker_minus = {
+  --         -- text = "●",
+  --         text = "",
+  --         hl = "@markup.list.markdown",
+  --       },
+  --       marker_plus = {
+  --         -- text = "●",
+  --         text = "",
+  --         hl = "@markup.list.markdown",
+  --       },
+  --       marker_star = {
+  --         -- text = "●",
+  --         text = "",
+  --         hl = "@markup.list.markdown",
+  --       },
+  --       marker_dot = {},
+  --     },
+  --     inline_codes = { enable = false },
+  --     headings = {
+  --       heading_1 = { style = "simple", hl = "Headline1" },
+  --       heading_2 = { style = "simple", hl = "Headline2" },
+  --       heading_3 = { style = "simple", hl = "Headline3" },
+  --       heading_4 = { style = "simple", hl = "Headline4" },
+  --       heading_5 = { style = "simple", hl = "Headline5" },
+  --       heading_6 = { style = "simple", hl = "Headline6" },
+  --     },
+  --     -- https://github.com/OXY2DEV/markview.nvim/issues/25#issuecomment-2224586784
+  --     options = {
+  --       on_enable = {
+  --         conceallevel = 2,
+  --         concealcursor = "",
+  --       },
+  --       on_disable = {
+  --         conceallevel = 0,
+  --         concealcursor = "",
+  --       },
+  --     },
+  --   },
+  --
+  --   ft = { "markdown", "norg", "rmd", "org" },
+  --   specs = {
+  --     "lukas-reineke/headlines.nvim",
+  --     enabled = false,
+  --   },
+  -- },
 
   -- https://github.com/nvim-orgmode/orgmode/blob/master/DOCS.md#mappings
   -- https://github.com/ales-tsurko/neovim-config/blob/f6d6e86c8d1b545d4de110513e2758edb9a31d6b/lua/extensions/orgmode.lua
