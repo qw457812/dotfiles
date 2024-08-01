592e980e8a36bb8e787ca18665ca770c4c1f992b
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-08-01 14:34:01 +0800

    fix(lazyvim-util): `replace_home_with_tilde(home)` return `~/.`

--- dot_config/nvim/lua/util/path.lua
+++ dot_config/nvim/lua/util/path.lua
@@ -4,13 +4,23 @@ local M = {}
 
 --- replace home directory with `~`
 --- https://github.com/ibhagwan/fzf-lua/blob/769b6636af07ea4587e6c06067d8fe9fb0629390/lua/fzf-lua/path.lua#L253
----@param path string
+---@param path string?
+---@return string?
 function M.replace_home_with_tilde(path)
+  if not path then
+    return nil
+  end
   -- vim.env.HOME
   -- os.getenv("HOME")
   local home = vim.uv.os_homedir()
-  if home and vim.startswith(path, home) then
-    -- path = path:gsub("^" .. home, "~")
+  if not home then
+    return path
+  end
+
+  -- path = path:gsub("^" .. home, "~") -- lua_regex_escape
+  if path == home then
+    path = "~"
+  elseif vim.startswith(path, home) then
     path = "~/" .. Path:new(path):make_relative(home)
   end
   return path
