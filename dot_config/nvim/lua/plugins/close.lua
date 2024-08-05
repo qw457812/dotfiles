156a9c6488705c9c07d06929d714691f08a61ac3
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-08-05 12:35:29 +0800

    chore(lazyvim-close): clean up FileType autocmds and buffer-closer.nvim

--- dot_config/nvim/lua/plugins/close.lua
+++ dot_config/nvim/lua/plugins/close.lua
@@ -64,55 +64,6 @@ vim.api.nvim_create_autocmd("User", {
   end,
 })
 
--- -- close some filetypes with close_key
--- -- ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/config/autocmds.lua
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = {
---     -- close_with_q by LazyVim
---     "PlenaryTestPopup",
---     "grug-far",
---     "help",
---     "lspinfo",
---     "notify",
---     "qf",
---     "spectre_panel",
---     "startuptime",
---     "tsplayground",
---     "neotest-output",
---     "checkhealth",
---     "neotest-summary",
---     "neotest-output-panel",
---     "dbout",
---     "gitsigns.blame",
---     -- close_key
---     "lazy",
---     "mason",
---     "Trans", -- JuanZoran/Trans.nvim
---   },
---   callback = function(event)
---     -- vim.bo[event.buf].buflisted = false
---     vim.keymap.set("n", close_key, "<cmd>close<cr>", {
---       buffer = event.buf,
---       silent = true,
---       desc = "Quit buffer",
---     })
---   end,
--- })
-
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = {
---     "dashboard", -- NOTE: not working in ../config/autocmds.lua, but works here or `opts` function of Lazy Plugin Spec
---     "leetcode.nvim", -- kawre/leetcode.nvim
---   },
---   callback = function(event)
---     vim.keymap.set("n", close_key, "<cmd>qa<cr>", {
---       buffer = event.buf,
---       silent = true,
---       desc = "Quit",
---     })
---   end,
--- })
-
 if close_key == "<bs>" then
   vim.api.nvim_create_autocmd("TermOpen", {
     pattern = "*lazygit",
@@ -129,18 +80,6 @@ if close_key == "<bs>" then
 end
 
 return {
-  -- -- Use the `close_buffer_or_window_or_exit` defined above instead of `psjay/buffer-closer.nvim`,
-  -- -- since it doesn't work in java's library files.
-  -- {
-  --   "psjay/buffer-closer.nvim",
-  --   keys = {
-  --     { close_key, desc = "Close buffer/window or Exit" },
-  --   },
-  --   opts = {
-  --     close_key = close_key,
-  --   },
-  -- },
-
   {
     "folke/edgy.nvim",
     optional = true,
