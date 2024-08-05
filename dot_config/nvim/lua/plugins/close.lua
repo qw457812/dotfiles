45a964e50f38dfbee79bf77843b54555e287e7c7
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-08-05 12:26:52 +0800

    refactor(lazyvim-close): compatible with non-listed manpages
    - Fixes "Cannot close last window" for non-listed buffers.
    - Using `vim.cmd("bd")` for floating windows.
    - Remove FileType autocmds, no longer needed.

--- dot_config/nvim/lua/plugins/close.lua
+++ dot_config/nvim/lua/plugins/close.lua
@@ -1,40 +1,65 @@
 -- close buffers, windows, or exit vim with the same single keypress
 local close_key = "<bs>" -- easy to reach for Glove80
 -- exit nvim
-local exit_key = "<leader>" .. close_key -- NOTE: conflict with "go up one level" of which-key
+local exit_key = "<leader>" .. close_key -- NOTE: would overwrite "go up one level" of which-key
 
 -- alternative to psjay/buffer-closer.nvim
-local function close_buffer_or_exit()
+-- copied from: https://github.com/psjay/buffer-closer.nvim/blob/74fec63c4c238b2cf6f61c40b47f869d442a8988/lua/buffer-closer/init.lua#L10
+local function close_buffer_or_window_or_exit()
   if vim.g.vscode then
     vim.cmd([[call VSCodeNotify('workbench.action.closeActiveEditor')]])
     return
   end
 
-  -- see `close_with_q` in ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/config/autocmds.lua
-  -- other non-listed buffers: lazy, mason, LazyVim.news.changelog(), JuanZoran/Trans.nvim
-  if not vim.bo.buflisted then
-    vim.cmd("close") -- Close Window
-    return
+  -- valid and not floating
+  -- https://github.com/echasnovski/mini.nvim/blob/af673d8523c5c2c5ff0a53b1e42a296ca358dcc7/lua/mini/animate.lua#L1397
+  local function normal_windows()
+    return vim.tbl_filter(function(w)
+      return vim.api.nvim_win_is_valid(w) and vim.api.nvim_win_get_config(w).relative == ""
+    end, vim.api.nvim_list_wins())
   end
 
-  -- copied from: https://github.com/psjay/buffer-closer.nvim/blob/74fec63c4c238b2cf6f61c40b47f869d442a8988/lua/buffer-closer/init.lua#L10
-  local listed_buffers = vim.tbl_filter(function(b)
-    return vim.bo[b].buflisted and vim.api.nvim_buf_is_valid(b)
-  end, vim.api.nvim_list_bufs())
+  local function listed_buffers()
+    return vim.tbl_filter(function(b)
+      return vim.bo[b].buflisted and vim.api.nvim_buf_is_valid(b)
+    end, vim.api.nvim_list_bufs())
+  end
 
-  if #listed_buffers > 1 then
-    -- TODO: Always use `vim.cmd("bd")` for floating windows?
-    -- vim.cmd("bd") -- Delete Buffer and Window
-    LazyVim.ui.bufremove() -- Delete Buffer
+  if vim.bo.buflisted then
+    -- 1. For listed buffers
+    if #listed_buffers() > 1 then
+      -- https://github.com/folke/which-key.nvim/blob/6c1584eb76b55629702716995cca4ae2798a9cca/lua/which-key/extras.lua#L53
+      local is_float = vim.api.nvim_win_get_config(0).relative ~= ""
+      if is_float then
+        vim.cmd("bd") -- Delete Buffer and Window
+      else
+        LazyVim.ui.bufremove() -- Delete Buffer
+      end
+    else
+      vim.cmd("qa")
+    end
   else
-    vim.cmd("qa")
+    -- 2. For non-listed buffers, including:
+    --    - some filetypes maintained by `close_with_q` autocmd-groups, see: ~/.local/share/nvim/lazy/LazyVim/lua/lazyvim/config/autocmds.lua
+    --    - manpages, see: https://github.com/LazyVim/LazyVim/blob/12818a6cb499456f4903c5d8e68af43753ebc869/lua/lazyvim/config/autocmds.lua#L84
+    --    - dashboard, leetcode.nvim
+    --    - others: lazy, mason, LazyVim.news.changelog(), JuanZoran/Trans.nvim, ...
+    if #normal_windows() > 1 then
+      vim.cmd("close") -- Close Window (Cannot close last window)
+    elseif #listed_buffers() > 0 then
+      -- eg. open manpage file directly while having other listed buffers
+      vim.cmd("bd") -- Delete Buffer and Window
+    else
+      -- eg. dashboard or open a single manpage file directly
+      vim.cmd("qa")
+    end
   end
 end
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "LazyVimKeymaps",
   callback = function()
-    vim.keymap.set("n", close_key, close_buffer_or_exit, { desc = "Delete Buffer or Exit" })
+    vim.keymap.set("n", close_key, close_buffer_or_window_or_exit, { desc = "Close buffer/window or Exit" })
     vim.keymap.set("n", exit_key, "<cmd>qa<cr>", { desc = "Quit All" })
   end,
 })
@@ -74,19 +99,19 @@ vim.api.nvim_create_autocmd("User", {
 --   end,
 -- })
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = {
-    "dashboard", -- NOTE: not working in ../config/autocmds.lua, but works here or `opts` function of Lazy Plugin Spec
-    "leetcode.nvim", -- kawre/leetcode.nvim
-  },
-  callback = function(event)
-    vim.keymap.set("n", close_key, "<cmd>qa<cr>", {
-      buffer = event.buf,
-      silent = true,
-      desc = "Quit",
-    })
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = {
+--     "dashboard", -- NOTE: not working in ../config/autocmds.lua, but works here or `opts` function of Lazy Plugin Spec
+--     "leetcode.nvim", -- kawre/leetcode.nvim
+--   },
+--   callback = function(event)
+--     vim.keymap.set("n", close_key, "<cmd>qa<cr>", {
+--       buffer = event.buf,
+--       silent = true,
+--       desc = "Quit",
+--     })
+--   end,
+-- })
 
 if close_key == "<bs>" then
   vim.api.nvim_create_autocmd("TermOpen", {
@@ -104,7 +129,7 @@ if close_key == "<bs>" then
 end
 
 return {
-  -- -- Use the `close_buffer_or_exit` defined above instead of `psjay/buffer-closer.nvim`,
+  -- -- Use the `close_buffer_or_window_or_exit` defined above instead of `psjay/buffer-closer.nvim`,
   -- -- since it doesn't work in java's library files.
   -- {
   --   "psjay/buffer-closer.nvim",
