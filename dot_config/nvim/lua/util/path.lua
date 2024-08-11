34bb7ed5faa15754c17cb5813de9eb5e88ae086a
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-08-11 18:03:54 +0800

    fix(lazyvim-util): `replace_home_with_tilde(home .. "/")` return `~/.`

--- dot_config/nvim/lua/util/path.lua
+++ dot_config/nvim/lua/util/path.lua
@@ -1,8 +1,7 @@
-local Path = require("plenary.path")
-
 local M = {}
 
---- replace home directory with `~`
+--- Replace home directory with '~'
+--- https://github.com/echasnovski/mini.files/blob/10ed64157ec45f176decefbdb0e2ba10cccd187f/lua/mini/files.lua#L2365
 --- https://github.com/ibhagwan/fzf-lua/blob/769b6636af07ea4587e6c06067d8fe9fb0629390/lua/fzf-lua/path.lua#L253
 ---@param path string?
 ---@return string?
@@ -10,19 +9,19 @@ function M.replace_home_with_tilde(path)
   if not path then
     return nil
   end
+
   -- vim.env.HOME
   -- os.getenv("HOME")
-  local home = vim.uv.os_homedir()
+  local home = (vim.uv or vim.loop).os_homedir()
   if not home then
     return path
   end
 
-  -- path = path:gsub("^" .. home, "~") -- lua_regex_escape
-  if path == home then
-    path = "~"
-  elseif vim.startswith(path, home) then
-    path = "~/" .. Path:new(path):make_relative(home)
-  end
+  -- if vim.startswith(path, home) then
+  --   local p = require("plenary.path"):new(path):make_relative(home)
+  --   path = p == "." and "~" or "~/" .. p
+  -- end
+  path = path:gsub("^" .. vim.pesc(home), "~")
   return path
 end
 
