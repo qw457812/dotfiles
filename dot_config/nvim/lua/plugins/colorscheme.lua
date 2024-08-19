a035c4af1964b4caa7d9669d73b3a971ab5468b9
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-08-19 14:05:04 +0800

    refactor(lazyvim-colorscheme): remove extras.colorscheme
    Move onedark from extras to plugins/colorscheme.lua, and remove others.

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -17,6 +17,7 @@ local function selectColorSchemeByTime()
     -- "tokyonight-night", -- similar to `tokyonight-storm` after `on_colors` opt
     "catppuccin-macchiato",
     "catppuccin-mocha",
+    "onedark",
   }
   local idx = tonumber(os.date("%S")) % #themes + 1
   local colorscheme = themes[idx]
@@ -151,6 +152,32 @@ return {
         }
       end,
     },
+  },
+
+  -- https://github.com/dhruvinsh/nvim/blob/bcc368b9e5013485fb01d46dfb2ea0037a2c9fc8/lua/orion/plugins/colors.lua#L9
+  -- alternative: olimorris/onedarkpro.nvim | https://github.com/appelgriebsch/Nv/blob/e9a584090a69a8d691f5eb051e76016b65dfc0b7/lua/plugins/extras/ui/onedarkpro-theme.lua
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    opts = function()
+      local style = "dark" -- dark(default), darker, cool, deep, warm, warmer, light
+      local util = require("onedark.util")
+      local colors = require("onedark.palette")[style]
+      local illuminate = util.darken(colors.grey, 0.8)
+      return {
+        style = style,
+        -- ~/.local/share/nvim/lazy/onedark.nvim/lua/onedark/palette.lua
+        highlights = {
+          IlluminatedWordRead = { bg = illuminate },
+          IlluminatedWordWrite = { bg = illuminate, fmt = "underline" },
+          CmpGhostText = { bg = "$bg0", fg = "$grey", fmt = "italic" },
+          DiagnosticUnnecessary = { fg = util.lighten(colors.grey, 0.7), fmt = "italic" },
+          -- TelescopePromptTitle = { bg = "$dark_cyan", fg = "white", fmt = "bold" },
+          -- TelescopeResultsTitle = { bg = "$dark_purple", fg = "white", fmt = "bold" },
+          -- TelescopePreviewTitle = { bg = "$dark_red", fg = "white", fmt = "bold" },
+        },
+      }
+    end,
   },
 
   {
