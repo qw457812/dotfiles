f962fa166141c7cf494b4c09420812535174f129
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-09-05 23:27:43 +0800

    feat(yazi): add git plugin

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,5 +1,7 @@
 -- https://github.com/yazi-rs/plugins/tree/main/full-border.yazi
 require("full-border"):setup()
+-- https://github.com/yazi-rs/plugins/tree/main/git.yazi
+require("git"):setup()
 
 -- TODO:
 -- https://github.com/Matt-FTW/dotfiles/blob/main/.config/yazi/init.lua
