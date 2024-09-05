12714a5c2b5ec6df8d1a003cf12936d3c8f7915c
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-09-06 06:08:09 +0800

    feat(yazi): add bookmarks plugin

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -2,6 +2,14 @@
 require("full-border"):setup()
 -- https://github.com/yazi-rs/plugins/tree/main/git.yazi
 require("git"):setup()
+-- https://github.com/dedukun/bookmarks.yazi
+require("bookmarks"):setup({
+	last_directory = { enable = true, persist = false },
+	persist = "all", -- none(default), all, vim
+	desc_format = "full", -- full(default), parent
+	file_pick_mode = "parent", -- hover(default), parent
+	notify = { enable = true },
+})
 
 -- TODO:
 -- https://github.com/Matt-FTW/dotfiles/blob/main/.config/yazi/init.lua
