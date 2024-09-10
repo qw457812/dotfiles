b6a6ea36a6ae31a7656a3a13c97667bd5535cf20
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-09-10 10:48:11 +0800

    refactor(lazyvim-util): color

--- dot_config/nvim/lua/util/color.lua
+++ dot_config/nvim/lua/util/color.lua
@@ -1,52 +1,71 @@
 ---@class util.color
 local M = {}
 
+-- copied from:
+-- https://github.com/folke/twilight.nvim/blob/1584c0b0a979b71fd86b18d302ba84e9aba85b1b/lua/twilight/util.lua
+-- https://github.com/folke/tokyonight.nvim/blob/4b386e66a9599057587c30538d5e6192e3d1c181/lua/tokyonight/util.lua
+-- https://github.com/catppuccin/nvim/blob/4fd72a9ab64b393c2c22b168508fd244877fec96/lua/catppuccin/utils/colors.lua
+
 M.bg = "#000000"
 M.fg = "#ffffff"
 
----@param c  string
-local function rgb(c)
-  c = string.lower(c)
-  return { tonumber(c:sub(2, 3), 16), tonumber(c:sub(4, 5), 16), tonumber(c:sub(6, 7), 16) }
+function M.hex2rgb(hex)
+  hex = string.lower(hex)
+  return tonumber(hex:sub(2, 3), 16), tonumber(hex:sub(4, 5), 16), tonumber(hex:sub(6, 7), 16)
 end
 
---- copied from: https://github.com/folke/tokyonight.nvim/blob/4b386e66a9599057587c30538d5e6192e3d1c181/lua/tokyonight/util.lua#L30
----@param foreground string foreground color
----@param background string background color
+function M.rgb2hex(r, g, b)
+  return string.format("#%02x%02x%02x", r, g, b)
+end
+
+---@param hex string foreground color
+---@param bg_hex string background color
 ---@param alpha number|string number between 0 and 1. 0 results in bg, 1 results in fg
-function M.blend(foreground, alpha, background)
+function M.blend(hex, bg_hex, alpha)
   alpha = type(alpha) == "string" and (tonumber(alpha, 16) / 0xff) or alpha
-  local bg = rgb(background)
-  local fg = rgb(foreground)
+  local fg = { M.hex2rgb(hex) }
+  local bg = { M.hex2rgb(bg_hex) }
 
   local blendChannel = function(i)
     local ret = (alpha * fg[i] + ((1 - alpha) * bg[i]))
     return math.floor(math.min(math.max(0, ret), 255) + 0.5)
   end
 
-  return string.format("#%02x%02x%02x", blendChannel(1), blendChannel(2), blendChannel(3))
+  return M.rgb2hex(blendChannel(1), blendChannel(2), blendChannel(3))
 end
 
-function M.darken(hex, amount, bg)
-  return M.blend(hex, amount, bg or M.bg)
+function M.blend_bg(hex, amount, bg)
+  return M.blend(hex, bg or M.bg, math.abs(amount))
 end
+-- function M.darken(hex, amount)
+--   local r, g, b = M.hex2rgb(hex)
+--   return M.rgb2hex(r * amount, g * amount, b * amount)
+-- end
+M.darken = M.blend_bg
 
-function M.lighten(hex, amount, fg)
-  return M.blend(hex, amount, fg or M.fg)
+function M.blend_fg(hex, amount, fg)
+  return M.blend(hex, fg or M.fg, math.abs(amount))
+end
+M.lighten = M.blend_fg
+
+function M.is_dark(hex)
+  local r, g, b = M.hex2rgb(hex)
+  -- Counting the perceptive luminance - human eye favors green color
+  local luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255
+  return luminance <= 0.5
 end
 
 ---@param hex string
 function M.to_neutral_gray(hex)
-  local r, g, b = unpack(rgb(hex))
+  local r, g, b = M.hex2rgb(hex)
 
   -- local avg = math.floor((r + g + b) / 3)
-  -- return string.format("#%02x%02x%02x", avg, avg, avg)
+  -- return M.rgb2hex(avg, avg, avg)
 
   -- https://github.com/killitar/obscure.nvim/blob/0e61b96a2c8551e73f8520b1f086d63f50d71bbd/lua/obscure/palettes/obscure.lua#L2
   -- https://github.com/mellow-theme/mellow.nvim/blob/5c8b4eaadf190f646f201322f96f00140b6b1a0b/lua/mellow/colors.lua#L5
-  local base_r, base_g, base_b = unpack(rgb("#161617"))
-  return string.format(
-    "#%02x%02x%02x",
+  local base_r, base_g, base_b = M.hex2rgb("#161617")
+  return M.rgb2hex(
     math.floor(base_r / (base_r + base_g + base_b) * (r + g + b)),
     math.floor(base_g / (base_r + base_g + base_b) * (r + g + b)),
     math.floor(base_b / (base_r + base_g + base_b) * (r + g + b))
