7e1444c161437aefe7b2fde27c98ec354b489dda
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-09-30 09:49:58 +0800

    refactor(lazyvim): move mini.align to formatting.lua

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -267,21 +267,6 @@ return {
   },
 
   {
-    "echasnovski/mini.align",
-    vscode = true,
-    opts = {
-      mappings = {
-        start = "", -- disabled since text-case.nvim uses `ga`
-        start_with_preview = "gA",
-      },
-    },
-    keys = {
-      -- { "ga", mode = { "n", "v" }, desc = "Align" },
-      { "gA", mode = { "n", "v" }, desc = "Align with Preview" },
-    },
-  },
-
-  {
     "dmtrKovalenko/caps-word.nvim",
     keys = {
       -- stylua: ignore
