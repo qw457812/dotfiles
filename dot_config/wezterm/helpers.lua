f2263284505fa76cc6082ef4fb169126401d8418
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-10-08 13:29:52 +0800

    chore(wezterm): update keys

---
+++ dot_config/wezterm/helpers.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+--- Extends a list-like table with the values of another list-like table.
+---
+--- NOTE: This mutates dst!
+---
+---@generic T: table
+---@param dst T List which will be modified and appended to
+---@param src table List from which values will be inserted
+---@param start integer? Start index on src. Defaults to 1
+---@param finish integer? Final index on src. Defaults to `#src`
+---@return T dst
+function M.list_extend(dst, src, start, finish)
+	for i = start or 1, finish or #src do
+		table.insert(dst, src[i])
+	end
+	return dst
+end
+
+return M
--- dot_config/wezterm/keys.lua
+++ dot_config/wezterm/keys.lua
@@ -2,6 +2,7 @@
 -- https://wezfurlong.org/wezterm/config/lua/keyassignment/index.html
 
 local wezterm = require("wezterm")
+local helpers = require("helpers")
 local act = wezterm.action
 
 local M = {}
@@ -106,6 +107,7 @@ end
 function M.apply_to_config(config)
 	-- -- tmux like
 	-- config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
+
 	-- wezterm show-keys
 	config.keys = {
 		-- Send "CTRL-B" to the terminal when pressing CTRL-B, CTRL-B
@@ -121,16 +123,17 @@ function M.apply_to_config(config)
 		split_nav("move", "CTRL", "k", "Up"),
 		split_nav("move", "CTRL", "l", "Right"),
 		-- -- cycles panes, then tabs, then windows
-		-- { mods = M.super, key = "w", action = next_pane_or_tab_or_window },
+		{ mods = M.super, key = "o", action = next_pane_or_tab_or_window },
 		{ mods = M.super, key = "w", action = act.CloseCurrentPane({ confirm = false }) },
 		-- Splits
-		{ mods = M.super, key = "Enter", action = smart_split },
+		{ mods = M.super, key = "s", action = smart_split },
 		{ mods = M.super, key = "\\", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 		{ mods = M.leader, key = "\\", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 		{ mods = M.super, key = "-", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 		{ mods = M.leader, key = "-", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
 		{ mods = M.super, key = "z", action = act.TogglePaneZoomState },
 		{ mods = M.leader, key = "z", action = act.TogglePaneZoomState },
+		{ mods = M.super, key = "Enter", action = act.RotatePanes("Clockwise") },
 		-- Tabs
 		{ mods = M.super, key = "j", action = act({ ActivateTabRelative = 1 }) },
 		{ mods = M.super, key = "k", action = act({ ActivateTabRelative = -1 }) },
@@ -138,13 +141,31 @@ function M.apply_to_config(config)
 		-- { mods = M.super, key = "u", action = act.ScrollByPage(-0.5) },
 		-- { mods = M.super, key = "d", action = act.ScrollByPage(0.5) },
 		-- Others
-		{ mods = M.super, key = "x", action = act.ActivateCopyMode },
-		{ mods = M.super, key = "s", action = act.QuickSelect },
+		{ mods = M.super, key = "Space", action = act.ActivateCopyMode },
 		{ mods = M.super, key = ":", action = act.ActivateCommandPalette },
+		{ mods = M.super, key = "p", action = act.QuickSelect },
 		{ mods = M.super, key = "d", action = act.ShowDebugOverlay },
 		{ mods = M.super, key = "u", action = open_url },
 		{ mods = M.super, key = "f", action = act.Search({ CaseInSensitiveString = "" }) },
 	}
+
+	config.key_tables = {}
+
+	-- Copy Mode
+	-- https://wezfurlong.org/wezterm/config/lua/wezterm.gui/default_key_tables.html
+	if wezterm.gui then
+		config.key_tables.copy_mode = helpers.list_extend(wezterm.gui.default_key_tables().copy_mode, {
+			{ mods = "SHIFT", key = "H", action = act.CopyMode("MoveToStartOfLineContent") },
+			{ mods = "SHIFT", key = "L", action = act.CopyMode("MoveToEndOfLineContent") },
+			{ mods = "NONE", key = "u", action = act.CopyMode({ MoveByPage = -0.5 }) },
+			{ mods = "NONE", key = "d", action = act.CopyMode({ MoveByPage = 0.5 }) },
+			{ mods = "CTRL", key = "u", action = act.CopyMode({ MoveByPage = -0.5 }) },
+			{ mods = "CTRL", key = "d", action = act.CopyMode({ MoveByPage = 0.5 }) },
+			{ mods = "NONE", key = "/", action = act.Search({ CaseInSensitiveString = "" }) },
+			{ mods = "NONE", key = "n", action = act.CopyMode("NextMatch") },
+			{ mods = "SHIFT", key = "N", action = act.CopyMode("PriorMatch") },
+		})
+	end
 end
 
 return M
