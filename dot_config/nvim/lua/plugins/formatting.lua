3be35acb7d601d2d2f6ac0fb608ffd10b9fc559a
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-10-13 20:40:10 +0800

    chore: up

--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -13,4 +13,21 @@ return {
       { "gA", mode = { "n", "v" }, desc = "Align with Preview" },
     },
   },
+
+  {
+    "echasnovski/mini.trailspace",
+    event = { "BufReadPost", "BufNewFile", "FileType" },
+    opts = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "dashboard",
+          "lazy",
+        },
+        callback = function(event)
+          vim.b[event.buf].minitrailspace_disable = true
+          vim.api.nvim_buf_call(event.buf, MiniTrailspace.unhighlight)
+        end,
+      })
+    end,
+  },
 }
