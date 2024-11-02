8aa236b6f2593585e4215dd966fa6d50659c7d4b
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-11-02 16:49:30 +0800

    feat(nvim-zen_mode): show bufferline in zen mode

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -362,19 +362,42 @@ return {
           twilight = { enabled = false }, -- bad performance
         },
         -- https://github.com/bleek42/dev-env-config-backup/blob/099eb0c4468a03bcafb6c010271818fe8a794816/src/Linux/config/nvim/lua/user/plugins/editor.lua#L27
-        on_open = function()
+        on_open = function(win)
           on_open()
           -- vim.g.user_zenmode_on = true -- require("zen-mode.view").is_open()
           vim.g.user_minianimate_disable_old = vim.g.minianimate_disable
           vim.g.minianimate_disable = true
           vim.g.user_winbar_old = vim.wo.winbar
           vim.wo.winbar = nil
+          -- show bufferline in zen mode
+          if package.loaded["bufferline"] and require("bufferline.utils").get_buf_count() > 1 then
+            vim.g.user_neotree_opened_old = vim.g.user_neotree_opened
+            if vim.g.user_neotree_opened then
+              require("neo-tree.command").execute({ action = "close" })
+            end
+            local view = require("zen-mode.view")
+            local layout = view.layout(view.opts)
+            vim.api.nvim_win_set_config(win, {
+              width = layout.width,
+              height = layout.height - 1,
+            })
+            vim.api.nvim_win_set_config(view.bg_win, {
+              width = vim.o.columns,
+              height = view.height() - 1,
+              row = 1,
+              col = layout.col,
+              relative = "editor",
+            })
+          end
         end,
         on_close = function()
           on_close()
           -- vim.g.user_zenmode_on = false
           vim.g.minianimate_disable = vim.g.user_minianimate_disable_old
           vim.wo.winbar = vim.g.user_winbar_old
+          if vim.g.user_neotree_opened_old then
+            require("neo-tree.command").execute({ action = "show" })
+          end
         end,
       })
     end,
