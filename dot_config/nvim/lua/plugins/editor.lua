33886c0f11a86567b4f89c3549b3b75d15d4ddab
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-11-11 00:39:36 +0800

    chore(nvim): up

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -258,9 +258,15 @@ return {
     -- vscode = true,
     keys = { { "u" }, { "<C-r>" } },
     opts = function()
-      -- link: Search IncSearch Substitute
-      vim.api.nvim_set_hl(0, "HighlightUndo", { default = true, link = "Substitute" })
-      vim.api.nvim_set_hl(0, "HighlightRedo", { default = true, link = "HighlightUndo" })
+      local function set_undo_hl()
+        -- link: Search IncSearch Substitute
+        vim.api.nvim_set_hl(0, "HighlightUndo", { default = true, link = "Substitute" })
+        vim.api.nvim_set_hl(0, "HighlightRedo", { default = true, link = "HighlightUndo" })
+      end
+
+      set_undo_hl()
+      vim.api.nvim_create_autocmd("ColorScheme", { callback = set_undo_hl })
+
       return {
         keymaps = {
           paste = {
@@ -291,7 +297,40 @@ return {
       { "zr", function() require("ufo").openFoldsExceptKinds() end },
       { "zm", function() require("ufo").closeFoldsWith() end },
     },
-    opts = {},
+    opts = function()
+      -- add number suffix of folded lines
+      local function virt_text_handler(virtText, lnum, endLnum, width, truncate)
+        local newVirtText = {}
+        local suffix = (" ⋯ %d "):format(endLnum - lnum)
+        local sufWidth = vim.fn.strdisplaywidth(suffix)
+        local targetWidth = width - sufWidth
+        local curWidth = 0
+        for _, chunk in ipairs(virtText) do
+          local chunkText = chunk[1]
+          local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+          if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+          else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, { chunkText, hlGroup })
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+              suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+          end
+          curWidth = curWidth + chunkWidth
+        end
+        table.insert(newVirtText, { suffix, "UfoFoldedEllipsis" })
+        return newVirtText
+      end
+
+      return {
+        fold_virt_text_handler = virt_text_handler,
+      }
+    end,
   },
 
   {
