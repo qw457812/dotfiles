08a2fe28da626ecfddaba7bba6fefc3c9b579ea8
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-11-22 05:43:00 +0800

    feat(nvim-markdown): add autolist.nvim

--- dot_config/nvim/lua/plugins/lang/markdown.lua
+++ dot_config/nvim/lua/plugins/lang/markdown.lua
@@ -16,4 +16,46 @@ return {
       -- },
     },
   },
+
+  {
+    "gaoDean/autolist.nvim",
+    ft = "markdown",
+    -- stylua: ignore
+    keys = {
+      { "<tab>",   "<cmd>AutolistTab<cr>",                ft = "markdown", mode = "i" },
+      { "<s-tab>", "<cmd>AutolistShiftTab<cr>",           ft = "markdown", mode = "i" },
+      { "<CR>",    "<CR><cmd>AutolistNewBullet<cr>",      ft = "markdown", mode = "i" },
+      { "o",       "o<cmd>AutolistNewBullet<cr>",         ft = "markdown" },
+      { "O",       "O<cmd>AutolistNewBulletBefore<cr>",   ft = "markdown" },
+      { "<CR>",    "<cmd>AutolistToggleCheckbox<cr><CR>", ft = "markdown" },
+      { "<M-r>",   "<cmd>AutolistRecalculate<cr>",        ft = "markdown" },
+      { ">>",      ">><cmd>AutolistRecalculate<cr>",      ft = "markdown" },
+      { "<<",      "<<<cmd>AutolistRecalculate<cr>",      ft = "markdown" },
+      { "dd",      "dd<cmd>AutolistRecalculate<cr>",      ft = "markdown" },
+      { "d",       "d<cmd>AutolistRecalculate<cr>",       ft = "markdown", mode = "v" },
+    },
+    opts = {},
+    config = function(_, opts)
+      require("autolist").setup(opts)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function(event)
+          -- cycle list types with dot-repeat
+          vim.keymap.set(
+            "n",
+            "].",
+            require("autolist").cycle_next_dr,
+            { expr = true, buffer = event.buf, desc = "Next List Type" }
+          )
+          vim.keymap.set(
+            "n",
+            "[.",
+            require("autolist").cycle_prev_dr,
+            { expr = true, buffer = event.buf, desc = "Prev List Type" }
+          )
+        end,
+      })
+    end,
+  },
 }
