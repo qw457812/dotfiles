47d8f1981381035315b608ba70f14031e998a5f3
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-11-22 17:44:50 +0800

    fix(nvim-autocmds): top-level `clear = true` clears autocmds on `Config Change Detected. Reloading...`

--- dot_config/nvim/lua/plugins/close.lua
+++ dot_config/nvim/lua/plugins/close.lua
@@ -1,9 +1,12 @@
 -- close buffers, windows, or exit vim with the same single keypress
-local close_key = "<bs>" -- easy to reach for Glove80
--- exit nvim
-local exit_key = "<leader>" .. close_key -- would overwrite "go up one level" of which-key, use `<S-bs>` if needed
+local close_key = vim.g.user_close_key or "<bs>" -- easy to reach for Glove80
+local is_bs = close_key:lower() == "<bs>"
 
-local augroup = vim.api.nvim_create_augroup("close_with_" .. close_key, { clear = true })
+-- exit nvim
+local exit_key = vim.g.user_exit_key or ("<leader>" .. close_key) -- would overwrite "go up one level" of which-key, use `<S-bs>` if needed
+
+-- do not use `clear = true` at the top-level, it will be triggered by lazy.nvim on `Config Change Detected. Reloading...`
+local augroup = vim.api.nvim_create_augroup("close_with_" .. close_key, { clear = false })
 
 -- alternative to psjay/buffer-closer.nvim
 -- copied from: https://github.com/psjay/buffer-closer.nvim/blob/74fec63c4c238b2cf6f61c40b47f869d442a8988/lua/buffer-closer/init.lua#L10
@@ -48,87 +51,179 @@ local function close_buffer_or_window_or_exit()
   end
 end
 
-vim.api.nvim_create_autocmd("User", {
-  group = augroup,
-  pattern = "LazyVimKeymaps",
-  once = true,
-  callback = function()
-    vim.keymap.set("n", close_key, close_buffer_or_window_or_exit, { desc = "Close buffer/window or Exit" })
-    vim.keymap.set("x", close_key, "<esc>", { desc = "Stop Visual Mode" })
-    vim.keymap.set("n", exit_key, "<cmd>qa<cr>", { desc = "Quit All" })
-    if close_key:lower() == "<bs>" then
-      vim.keymap.set("c", close_key, function()
-        if vim.fn.getcmdline() ~= "" then
-          return close_key
-        end
-      end, { expr = true, desc = "<bs> does not leave cmdline" })
-    end
-  end,
-})
-
--- close some filetypes with close_key
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup,
-  pattern = {
-    "Avante",
-    "AvanteInput",
-  },
-  callback = function(event)
-    vim.keymap.set("n", close_key, "<cmd>close<cr>", {
-      buffer = event.buf,
-      silent = true,
-      desc = "Quit buffer",
-    })
-  end,
-})
-
--- see: `:h q:`
-vim.api.nvim_create_autocmd("CmdWinEnter", {
-  group = augroup,
-  callback = function(event)
-    vim.keymap.set("n", close_key, "<cmd>q<cr>", {
-      buffer = event.buf,
-      silent = true,
-      desc = "Close command-line window",
-    })
-  end,
-})
-
-if close_key:lower() == "<bs>" then
-  vim.api.nvim_create_autocmd("TermOpen", {
-    group = augroup,
-    pattern = "*lazygit",
-    callback = function(event)
-      -- mapped <c-h> to quit in lazygit/config.yml via `quitWithoutChangingDirectory: <backspace>`
-      -- when typing a commit message in lazygit, <c-h> and <bs> do the same thing
-      vim.keymap.set("t", close_key, "<C-h>", {
-        buffer = event.buf,
-        silent = true,
-        desc = "Quit Lazygit",
-      })
-    end,
-  })
-end
-
-vim.defer_fn(function()
-  if vim.g.db_ui_tmp_query_location then
-    vim.api.nvim_create_autocmd("BufNewFile", {
-      pattern = vim.g.db_ui_tmp_query_location .. "/*",
-      callback = function(event)
-        vim.keymap.set("n", close_key, function()
-          -- do not ask for saving changes
-          Snacks.bufdelete({ buf = event.buf, force = true })
-        end, {
-          buffer = event.buf,
-          silent = true,
-          desc = "Quit buffer",
-        })
-      end,
-    })
-  end
-end, 100) -- wait for vim-dadbod-ui to `init`, otherwise vim.g.db_ui_tmp_query_location is nil
-
 return {
+  {
+    "LazyVim/LazyVim",
+    keys = {
+      { close_key, close_buffer_or_window_or_exit, desc = "Close buffer/window or Exit" },
+      { close_key, mode = "x", "<esc>", desc = "Stop Visual Mode" },
+      { exit_key, "<cmd>qa<cr>", desc = "Quit All" },
+    },
+    opts = function()
+      -- vim.api.nvim_create_autocmd("User", {
+      --   group = augroup,
+      --   pattern = "LazyVimKeymaps",
+      --   once = true,
+      --   callback = function()
+      --     if is_bs then
+      --       if not package.loaded["mini.pairs"] then
+      --         vim.keymap.set("c", "<bs>", function()
+      --           if vim.fn.getcmdline() ~= "" then
+      --             return "<bs>"
+      --           end
+      --         end, { expr = true, desc = "<bs> does not leave cmdline" })
+      --       end
+      --
+      --       if LazyVim.has("mini.pairs") then
+      --         LazyVim.on_load(
+      --           "mini.pairs",
+      --           vim.schedule_wrap(function()
+      --             local pairs = require("mini.pairs")
+      --             local c_pairs_bs = pairs.config.modes.command
+      --             -- see: https://github.com/echasnovski/mini.pairs/blob/7e834c5937d95364cc1740e20d673afe2d034cdb/lua/mini/pairs.lua#L574C5-L576C54
+      --             vim.keymap.set("c", "<bs>", function()
+      --               if vim.fn.getcmdline() ~= "" then
+      --                 return c_pairs_bs and pairs.bs() or "<bs>"
+      --               end
+      --             end, {
+      --               expr = true,
+      --               silent = false,
+      --               replace_keycodes = not c_pairs_bs,
+      --               desc = string.format("%s does not leave cmdline", c_pairs_bs and "MiniPairs.bs()" or "<bs>"),
+      --             })
+      --           end)
+      --         )
+      --       end
+      --     end
+      --   end,
+      -- })
+
+      if is_bs then
+        -- if not package.loaded["mini.pairs"] then
+        --   vim.keymap.set("c", "<bs>", function()
+        --     if vim.fn.getcmdline() ~= "" then
+        --       return "<bs>"
+        --     end
+        --   end, { expr = true, desc = "<bs> does not leave cmdline" })
+        -- end
+        --
+        -- if LazyVim.has("mini.pairs") then
+        --   LazyVim.on_load(
+        --     "mini.pairs",
+        --     vim.schedule_wrap(function()
+        --       local pairs = require("mini.pairs")
+        --       local c_pairs_bs = pairs.config.modes.command
+        --       -- see: https://github.com/echasnovski/mini.pairs/blob/7e834c5937d95364cc1740e20d673afe2d034cdb/lua/mini/pairs.lua#L574C5-L576C54
+        --       vim.keymap.set("c", "<bs>", function()
+        --         if vim.fn.getcmdline() ~= "" then
+        --           return c_pairs_bs and pairs.bs() or "<bs>"
+        --         end
+        --       end, {
+        --         expr = true,
+        --         silent = false,
+        --         replace_keycodes = not c_pairs_bs,
+        --         desc = string.format("%s does not leave cmdline", c_pairs_bs and "MiniPairs.bs()" or "<bs>"),
+        --       })
+        --     end)
+        --   )
+        -- end
+
+        vim.api.nvim_create_autocmd("TermOpen", {
+          group = augroup,
+          pattern = "*lazygit",
+          callback = function(event)
+            -- mapped <c-h> to quit in lazygit/config.yml via `quitWithoutChangingDirectory: <backspace>`
+            -- when typing a commit message in lazygit, <c-h> and <bs> do the same thing
+            vim.keymap.set("t", "<bs>", "<C-h>", {
+              buffer = event.buf,
+              silent = true,
+              desc = "Quit Lazygit",
+            })
+          end,
+        })
+      end
+
+      -- see: `:h q:`
+      vim.api.nvim_create_autocmd("CmdWinEnter", {
+        group = augroup,
+        callback = function(event)
+          vim.keymap.set("n", close_key, "<cmd>q<cr>", {
+            buffer = event.buf,
+            silent = true,
+            desc = "Close command-line window",
+          })
+        end,
+      })
+
+      -- close some filetypes with close_key
+      vim.api.nvim_create_autocmd("FileType", {
+        group = augroup,
+        pattern = {
+          "Avante",
+          "AvanteInput",
+        },
+        callback = function(event)
+          vim.keymap.set("n", close_key, "<cmd>close<cr>", {
+            buffer = event.buf,
+            silent = true,
+            desc = "Quit buffer",
+          })
+        end,
+      })
+
+      if LazyVim.has("vim-dadbod-ui") then
+        LazyVim.on_load("vim-dadbod-ui", function()
+          if vim.g.db_ui_tmp_query_location then
+            vim.api.nvim_create_autocmd("BufNewFile", {
+              group = augroup,
+              pattern = vim.g.db_ui_tmp_query_location .. "/*",
+              callback = function(event)
+                vim.keymap.set("n", close_key, function()
+                  -- do not ask for saving changes
+                  Snacks.bufdelete({ buf = event.buf, force = true })
+                end, {
+                  buffer = event.buf,
+                  silent = true,
+                  desc = "Quit buffer",
+                })
+              end,
+            })
+          end
+        end)
+      end
+    end,
+  },
+
+  -- {
+  --   "echasnovski/mini.pairs",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     if not is_bs then
+  --       return
+  --     end
+  --
+  --     LazyVim.on_load(
+  --       "mini.pairs",
+  --       vim.schedule_wrap(function()
+  --         local pairs = require("mini.pairs")
+  --         local c_pairs_bs = (opts.modes or {}).command or pairs.config.modes.command
+  --
+  --         -- see: https://github.com/echasnovski/mini.pairs/blob/7e834c5937d95364cc1740e20d673afe2d034cdb/lua/mini/pairs.lua#L574C5-L576C54
+  --         vim.keymap.set("c", "<bs>", function()
+  --           if vim.fn.getcmdline() ~= "" then
+  --             return c_pairs_bs and pairs.bs() or "<bs>"
+  --           end
+  --         end, {
+  --           expr = true,
+  --           silent = false,
+  --           replace_keycodes = not c_pairs_bs,
+  --           desc = string.format("%s does not leave cmdline", c_pairs_bs and "MiniPairs.bs()" or "<bs>"),
+  --         })
+  --       end)
+  --     )
+  --   end,
+  -- },
+
   {
     "folke/edgy.nvim",
     optional = true,
