22513a7edb40629145077edd8d3f96f0bf5406bb
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-12-18 18:50:40 +0800

    feat(nvim-lsp): move mason-lock to extras

---
+++ dot_config/nvim/lua/plugins/extras/lsp/mason-lock.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "zapling/mason-lock.nvim",
+      cmd = { "MasonLock", "MasonLockRestore" },
+      opts = {},
+    },
+  },
+}
