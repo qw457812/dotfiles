3d46cdf3a2db87f7ac1d043f3fb865fd0c5a1637
qw457812 <37494864+qw457812@users.noreply.github.com>
2024-12-27 11:33:13 +0800

    feat(nvim-aider): vim mode

--- dot_config/nvim/lua/plugins/extras/ai/aider.lua
+++ dot_config/nvim/lua/plugins/extras/ai/aider.lua
@@ -29,26 +29,46 @@ return {
     opts = function()
       local defaults = require("nvim_aider.config").defaults
 
-      Snacks.config.style(defaults.win.style, {
-        keys = {
-          aider_close = {
-            toggle_key, -- <esc>
-            function(self)
-              self:hide()
-            end,
-            mode = "t",
-            desc = "Close",
-          },
-        },
-      })
-
       return {
         args = vim.list_extend(vim.deepcopy(defaults.args), {
+          "--dark-mode",
+          "--vim",
           "--model",
           "groq/llama-3.3-70b-versatile",
         }),
+        ---@module "snacks"
+        ---@type snacks.win.Config
+        ---@diagnostic disable-next-line: missing-fields
         win = {
           position = "float",
+          keys = {
+            aider_close = {
+              toggle_key, -- <esc>
+              function(self)
+                self:hide()
+              end,
+              mode = "t",
+              desc = "Close",
+            },
+            -- copied from: https://github.com/folke/snacks.nvim/blob/98df370703b3c47a297988f3e55ce99628639590/lua/snacks/terminal.lua#L45
+            term_normal = {
+              "<esc>",
+              function(self)
+                self.esc_timer = self.esc_timer or (vim.uv or vim.loop).new_timer()
+                if self.esc_timer:is_active() then
+                  self.esc_timer:stop()
+                  vim.api.nvim_feedkeys(vim.keycode("a"), "n", false) -- add this line for `--vim`
+                  vim.cmd("stopinsert")
+                else
+                  self.esc_timer:start(200, 0, function() end)
+                  return "<esc>"
+                end
+              end,
+              mode = "t",
+              expr = true,
+              desc = "Double escape to normal mode",
+            },
+          },
         },
       }
     end,
