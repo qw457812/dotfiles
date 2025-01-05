a3dd105d8ed3648b335604647f7186da4035a6bb
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-01-05 23:41:35 +0800

    feat(nvim-rime): only imap alphabet when rime is enabled

--- dot_config/nvim/lua/plugins/rime.lua
+++ dot_config/nvim/lua/plugins/rime.lua
@@ -3,100 +3,6 @@ if not (vim.fn.executable("rime_ls") == 1 and LazyVim.has("blink.cmp")) then
   return {}
 end
 
-local toggle_key = "<c-.>" -- TODO: better keymap
-
-local function rime_on_attach(client, _)
-  vim.api.nvim_create_user_command("RimeToggle", function()
-    client.request("workspace/executeCommand", { command = "rime-ls.toggle-rime" }, function(_, result, ctx, _)
-      if ctx.client_id == client.id then
-        vim.g.rime_enabled = result
-      end
-    end)
-  end, { nargs = 0 })
-
-  local max_code = 4
-  local alphabet = "abcdefghijklmnopqrstuvwxyz"
-  local reverse_lookup_prefix = "`"
-  local mapped_punc = {
-    [","] = "，",
-    ["."] = "。",
-    [";"] = "；",
-    ["?"] = "？",
-    ["\\"] = "、",
-    -- [":"] = "：", -- disable it in favor of secondary election
-  }
-
-  local function match_alphabet(txt)
-    return string.match(txt, "^[" .. alphabet .. "]+$") ~= nil
-  end
-
-  -- auto accept when there is only one rime item after inputting a number
-  require("blink.cmp.completion.list").show_emitter:on(function(event)
-    if not vim.g.rime_enabled then
-      return
-    end
-    local col = vim.fn.col(".") - 1
-    if event.context.line:sub(col, col):match("%d") == nil then
-      return
-    end
-    local rime_item_index = U.rime_ls.get_n_rime_item_index(2, event.items)
-    if #rime_item_index ~= 1 then
-      return
-    end
-    require("blink.cmp").accept({ index = rime_item_index[1] })
-  end)
-
-  -- Toggle rime, this will toggle Chinese punctuations too
-  U.keymap({ "n", "i" }, toggle_key, function()
-    -- We must check the status before the toggle
-    if vim.g.rime_enabled then
-      for k, _ in pairs(mapped_punc) do
-        vim.keymap.del("i", k .. "<space>")
-      end
-    else
-      for k, v in pairs(mapped_punc) do
-        U.keymap("i", k .. "<space>", v)
-      end
-    end
-    vim.cmd("RimeToggle")
-  end)
-
-  -- Select first entry when typing more than max_code
-  for i = 1, #alphabet do
-    local k = alphabet:sub(i, i)
-    U.keymap("i", k, function()
-      local cursor_column = vim.api.nvim_win_get_cursor(0)[2]
-      local confirmed = false
-      if vim.g.rime_enabled and cursor_column >= max_code then
-        local content_before_cursor = string.sub(vim.api.nvim_get_current_line(), 1, cursor_column)
-        local code = string.sub(content_before_cursor, cursor_column - max_code + 1, cursor_column)
-        if match_alphabet(code) then
-          -- This is for tiger-code users using '`' as reverse look up
-          if not string.match(content_before_cursor, reverse_lookup_prefix .. "[" .. alphabet .. "]*$") then
-            local first_rime_item_index = U.rime_ls.get_n_rime_item_index(1)
-            if #first_rime_item_index ~= 1 then
-              -- clear the wrong code
-              for _ = 1, max_code do
-                vim.api.nvim_feedkeys(vim.keycode("<bs>"), "n", false)
-              end
-            else
-              require("blink.cmp").accept({ index = first_rime_item_index[1] })
-              confirmed = true
-            end
-          end
-        end
-      end
-      if confirmed then
-        vim.schedule(function()
-          vim.api.nvim_feedkeys(vim.keycode(k), "n", false)
-        end)
-      else
-        vim.api.nvim_feedkeys(vim.keycode(k), "n", false)
-      end
-    end)
-  end
-end
-
 -- https://github.com/wlh320/rime-ls/blob/4986d4d765870846f689e1e06dc9baa2ac2aff34/doc/nvim-with-blink.md
 -- https://github.com/Kaiser-Yang/dotfiles/tree/bdda941b06cce5c7505bc725f09dd3fa17763730
 -- https://github.com/wlh320/wlh-dotfiles/blob/85d41a30588642617177374b4cea2ec96c1b2740/config/nvim/lua/rime.lua
@@ -116,24 +22,11 @@ return {
             always_incomplete = true,
             long_filter_text = true,
           },
-          on_attach = rime_on_attach,
+          on_attach = U.rime_ls.on_attach,
         },
       },
       setup = {
-        rime_ls = function()
-          U.rime_ls.setup({
-            filetype = vim.g.rime_ls_support_filetype,
-          })
-
-          -- HACK: rime_ls is visible in blink.cmp on startup
-          LazyVim.lsp.on_attach(function()
-            vim.defer_fn(function()
-              vim.cmd("RimeToggle")
-            end, 100)
-            ---@diagnostic disable-next-line: redundant-return-value
-            return true -- don't mess up toggle
-          end, "rime_ls")
-        end,
+        rime_ls = U.rime_ls.setup,
       },
     },
   },
@@ -150,11 +43,11 @@ return {
             if not vim.g.rime_enabled then
               return false
             end
-            local rime_item_index = U.rime_ls.get_n_rime_item_index(1)
-            if #rime_item_index ~= 1 then
+            local indices = U.rime_ls.top_k_rime_item_indices(1)
+            if #indices ~= 1 then
               return false
             end
-            return cmp.accept({ index = rime_item_index[1] })
+            return cmp.accept({ index = indices[1] })
           end,
           "fallback",
         },
@@ -163,11 +56,11 @@ return {
             if not vim.g.rime_enabled then
               return false
             end
-            local rime_item_index = U.rime_ls.get_n_rime_item_index(2)
-            if #rime_item_index ~= 2 then
+            local indices = U.rime_ls.top_k_rime_item_indices(2)
+            if #indices ~= 2 then
               return false
             end
-            return cmp.accept({ index = rime_item_index[2] })
+            return cmp.accept({ index = indices[2] })
           end,
           "fallback",
         },
@@ -176,11 +69,11 @@ return {
             if not vim.g.rime_enabled then
               return false
             end
-            local rime_item_index = U.rime_ls.get_n_rime_item_index(3)
-            if #rime_item_index ~= 3 then
+            local indices = U.rime_ls.top_k_rime_item_indices(3)
+            if #indices ~= 3 then
               return false
             end
-            return cmp.accept({ index = rime_item_index[3] })
+            return cmp.accept({ index = indices[3] })
           end,
           "fallback",
         },
@@ -202,7 +95,7 @@ return {
               ---@param item blink.cmp.CompletionItem
               return vim.tbl_filter(function(item)
                 return item.kind ~= require("blink.cmp.types").CompletionItemKind.Text
-                  or (U.rime_ls.is_rime_item(item) and U.rime_ls.rime_item_acceptable(item))
+                  or U.rime_ls.is_valid_rime_item(item)
               end, items)
             end,
           },
