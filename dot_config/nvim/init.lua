85c88f2039abf3108fd39e9c20519602062bc0c4
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-01-08 13:30:02 +0800

    feat(nvim-snacks): define _G.dd in init.lua to make it available on startup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,4 +1,15 @@
 _G.U = require("util")
 
+_G.dd = function(...)
+  require("snacks.debug").inspect(...)
+end
+_G.bt = function()
+  require("snacks.debug").backtrace()
+end
+_G.p = function(...)
+  require("snacks.debug").profile(...)
+end
+vim.print = _G.dd -- override print to use snacks for `:=` command
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
