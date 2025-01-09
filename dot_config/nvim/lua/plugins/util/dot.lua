878732926f9bf47044684f292db36f495df143cf
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-01-10 02:39:07 +0800

    refactor(nvim): move csv.lua to extras.lang, zsh.lua to util.dot.lua

--- dot_config/nvim/lua/plugins/util/dot.lua
+++ dot_config/nvim/lua/plugins/util/dot.lua
@@ -1,4 +1,18 @@
 return {
+  -- zsh
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.zsh = { "shfmt" }
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = { ensure_installed = { "shfmt" } },
+  },
+
   {
     "LazyVim/LazyVim",
     opts = function()
