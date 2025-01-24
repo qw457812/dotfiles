2f681b2aaf385152800fd3a51c77b2238db20fda
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-01-24 14:25:01 +0800

    fix(nvim-esc): has_notif check

--- dot_config/nvim/lua/plugins/picker.lua
+++ dot_config/nvim/lua/plugins/picker.lua
@@ -159,6 +159,8 @@ return {
               ["<Down>"] = "history_forward",
               i_up = { "<Up>", "list_up", mode = "i", expr = true },
               i_down = { "<Down>", "list_down", mode = "i", expr = true },
+              ["<c-h>"] = { "preview_scroll_left", mode = { "i", "n" } },
+              ["<c-l>"] = { "preview_scroll_right", mode = { "i", "n" } },
             },
           },
           list = {
