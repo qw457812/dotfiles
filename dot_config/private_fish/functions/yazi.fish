9095de3569dd9c51722bc269ba8e7cb4c1c027f8
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-02-13 10:16:49 +0800

    fix(fish-yazi): `cd -` not working after using `yazi`

--- dot_config/private_fish/functions/yazi.fish
+++ dot_config/private_fish/functions/yazi.fish
@@ -3,7 +3,7 @@ function yazi --wraps=yazi
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     command yazi $argv --cwd-file="$tmp"
     if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-        builtin cd -- "$cwd"
+        cd -- "$cwd" # using `cd` instead of `builtin cd` to maintain `cd -` functionality
     end
     rm -f -- "$tmp"
 end
