2e5d14b6a6865bb32fbe623f36065adb456ee0ef
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-02-16 22:37:18 +0800

    feat(nvim-snacks): better `h`/`l` for explorer

--- dot_config/nvim/lua/plugins/explorer/snacks.lua
+++ dot_config/nvim/lua/plugins/explorer/snacks.lua
@@ -37,6 +37,52 @@ return {
                   end
                 end
               end,
+              reset_input = function(picker)
+                picker.input:set("", "")
+              end,
+              -- copied from: https://github.com/folke/snacks.nvim/blob/938aee4a02119ad693a67c38b64a9b3232a72565/lua/snacks/explorer/actions.lua#L104-L114
+              parent_or_close = function(picker, item)
+                local Actions = require("snacks.explorer.actions")
+                local Tree = require("snacks.explorer.tree")
+
+                if not item then
+                  return
+                end
+                local dir = picker:dir()
+                if dir == picker:cwd() then
+                  Actions.actions.explorer_up(picker)
+                end
+                if item.dir and not item.open then
+                  dir = vim.fs.dirname(dir)
+                end
+                Tree:close(dir)
+                Actions.update(picker, { target = dir, refresh = true })
+              end,
+              -- copied from: https://github.com/folke/snacks.nvim/blob/938aee4a02119ad693a67c38b64a9b3232a72565/lua/snacks/explorer/actions.lua#L276-L287
+              child_or_open = function(picker, item, action)
+                local Actions = require("snacks.explorer.actions")
+                local Tree = require("snacks.explorer.tree")
+
+                if not item then
+                  return
+                elseif picker.input.filter.meta.searching then
+                  Actions.update(picker, { target = item.file })
+                elseif item.dir then
+                  local dir = Tree:dir(item.file)
+                  local node = Tree:find(dir)
+                  if node.open then
+                    Snacks.picker.actions.list_down(picker)
+                  else
+                    Tree:open(dir)
+                    Actions.update(picker, { refresh = true })
+                  end
+                else
+                  Snacks.picker.actions.jump(picker, item, action)
+                  if vim.g.user_explorer_auto_close or too_narrow() then
+                    picker:close()
+                  end
+                end
+              end,
             },
             win = {
               list = {
@@ -50,6 +96,8 @@ return {
                     end,
                     desc = "Clear UI or Close or Unfocus",
                   },
+                  ["h"] = "parent_or_close",
+                  ["l"] = "child_or_open",
                   ["f"] = "focus_input", -- filter
                 },
               },
@@ -59,10 +107,10 @@ return {
                     "<Esc>",
                     function(self)
                       if not U.keymap.clear_ui_esc({ close = false }) then
-                        self:execute("focus_list")
+                        self:execute(self:line() ~= "" and "reset_input" or "focus_list")
                       end
                     end,
-                    desc = "Clear UI or Focus List",
+                    desc = "Clear UI or Reset Input or Focus List",
                   },
                 },
               },
@@ -84,10 +132,7 @@ return {
         elseif picker:is_focused() then
           picker:action("close_or_unfocus")
         else
-          local win = picker.layout.wins.list
-          if win then
-            win:focus()
-          end
+          picker:action("focus_list")
         end
       end
 
