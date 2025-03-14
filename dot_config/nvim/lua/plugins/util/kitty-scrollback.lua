ef1f47e9358667c465652a2d2ad8540015ce7d8d
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-03-14 23:19:19 +0800

    feat(kitty-scrollback): custom lualine.nvim

--- dot_config/nvim/lua/plugins/util/kitty-scrollback.lua
+++ dot_config/nvim/lua/plugins/util/kitty-scrollback.lua
@@ -11,7 +11,7 @@ local enabled = {
   -- the plugins below are optional
   "which-key.nvim",
   "tokyonight.nvim",
-  "lualine.nvim", -- TODO: custom
+  "lualine.nvim",
   "mini.icons", -- for lualine.nvim
   "noice.nvim",
   "nui.nvim", -- for noice.nvim
@@ -50,8 +50,34 @@ return {
     "LazyVim/LazyVim",
     config = function(_, opts)
       opts = opts or {}
-      opts.colorscheme = LazyVim.has("tokyonight.nvim") and "tokyonight-night" or function() end
+      opts.colorscheme = LazyVim.has("tokyonight.nvim") and "tokyonight-moon" or function() end
       require("lazyvim").setup(opts)
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    config = function(_, opts)
+      -- stylua: ignore start
+      opts.sections.lualine_a = { { function() return "kitty" end } }
+      opts.sections.lualine_b = { { function() return "scrollback" end } }
+      opts.sections.lualine_c = {
+        {
+          function() return "󰄛 " end,
+          color = function() return { fg = Snacks.util.color("MiniIconsYellow") } end,
+        },
+      }
+      opts.sections.lualine_x = {
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = function() return { fg = Snacks.util.color("Statement") } end,
+        },
+      }
+      -- stylua: ignore end
+      opts.sections.lualine_y = { { "progress" } }
+      opts.extensions = {}
+      require("lualine").setup(opts)
     end,
   },
   { "RRethy/vim-illuminate", optional = true, event = "VeryLazy" },
