d7930e869f16a578fc8f27d260b9c6ceaf0ff578
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-04-03 14:41:45 +0800

    feat: enable `cv01` feature for Maple Mono Font

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -81,6 +81,8 @@ if vim.g.neovide then
 
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_input_macos_option_key_is_meta = "both"
-  vim.g.neovide_cursor_vfx_mode = "railgun" -- railgun, torpedo, pixiedust
-  vim.g.neovide_transparency = vim.g.user_transparent_background and 0.0 or vim.g.neovide_transparency
+  vim.g.neovide_scroll_animation_length = 0.150
+  vim.g.neovide_cursor_animation_length = 0.04
+  -- vim.g.neovide_cursor_vfx_mode = "railgun" -- railgun, torpedo, pixiedust
+  vim.g.neovide_opacity = vim.g.user_transparent_background and 0.0 or vim.g.neovide_opacity
 end
