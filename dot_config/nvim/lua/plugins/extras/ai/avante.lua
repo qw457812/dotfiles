4d762148beae27e865b6408d97408236d4da9670
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-04-08 16:35:10 +0800

    feat(nvim-ai): add extras.ai.mcphub with avante

--- dot_config/nvim/lua/plugins/extras/ai/avante.lua
+++ dot_config/nvim/lua/plugins/extras/ai/avante.lua
@@ -261,6 +261,13 @@ return {
           api_key_name = "OPENROUTER_API_KEY",
           model = "anthropic/claude-3.7-sonnet",
         },
+        ---@type AvanteSupportedProvider
+        openrouter_gemini = {
+          __inherited_from = "openai",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+          model = "google/gemini-2.5-pro-preview-03-25",
+        },
       },
       hints = { enabled = false },
     },
