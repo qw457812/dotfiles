395ff4c36f79e2efecc7d35ff2786fc35ef0393e
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-04-16 13:59:27 +0800

    feat(nvim): enable helpview.nvim

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -35,7 +35,6 @@ return {
 
   {
     "OXY2DEV/helpview.nvim",
-    enabled = false,
     ft = "help",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
@@ -44,8 +43,25 @@ return {
       end,
     },
     keys = {
-      { "<leader>uH", "<cmd>Helpview toggleAll<cr>", desc = "Helpview" },
+      { "<leader>uH", "<cmd>Helpview Toggle<cr>", desc = "Helpview" },
     },
+    opts = {
+      preview = {
+        icon_provider = "mini",
+      },
+    },
+    config = function(_, opts)
+      require("helpview").setup(opts)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("helpview_fix_lazy", { clear = true }),
+        pattern = "help",
+        once = true,
+        callback = function()
+          vim.cmd("Helpview attach")
+        end,
+      })
+    end,
   },
 
   {
