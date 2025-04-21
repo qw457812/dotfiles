0c307e3b4c15a2ab2db68f66a3b885abb058b9de
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-04-21 10:49:27 +0800

    fix(nvim-pantran): proxy for kitty scrollback_pager

--- dot_config/nvim/lua/plugins/lang/sql.lua
+++ dot_config/nvim/lua/plugins/lang/sql.lua
@@ -93,5 +93,52 @@ return {
     end,
   },
 
+  -- -- sqlfluff -> sqruff
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = function(_, opts)
+  --     for i, pkg in ipairs(opts.ensure_installed) do
+  --       if pkg == "sqlfluff" then
+  --         table.remove(opts.ensure_installed, i)
+  --         break
+  --       end
+  --     end
+  --     table.insert(opts.ensure_installed, "sqruff")
+  --   end,
+  -- },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     for _, ft in ipairs(sql_ft) do
+  --       opts.linters_by_ft[ft] = opts.linters_by_ft[ft] or {}
+  --       for i, linter in ipairs(opts.linters_by_ft[ft]) do
+  --         if linter == "sqlfluff" then
+  --           table.remove(opts.linters_by_ft[ft], i)
+  --           break
+  --         end
+  --       end
+  --       table.insert(opts.linters_by_ft[ft], "sqruff")
+  --     end
+  --   end,
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     opts.formatters.sqlfluff = nil
+  --     for _, ft in ipairs(sql_ft) do
+  --       opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+  --       for i, formatter in ipairs(opts.formatters_by_ft[ft]) do
+  --         if formatter == "sqlfluff" then
+  --           table.remove(opts.formatters_by_ft[ft], i)
+  --           break
+  --         end
+  --       end
+  --       table.insert(opts.formatters_by_ft[ft], "sqruff")
+  --     end
+  --   end,
+  -- },
+
   -- { "jsborjesson/vim-uppercase-sql", ft = "sql" },
 }
