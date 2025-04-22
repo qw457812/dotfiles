0ac14b2b77b5b7755b3dcd83667f2ea25772d9f8
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-04-23 00:48:52 +0800

    fix(fish): complete for `gi`

--- dot_config/private_fish/functions/gi.fish
+++ dot_config/private_fish/functions/gi.fish
@@ -9,7 +9,8 @@ function __complete_gi
     if ! set -q __COMPLETE_GI
         set -g __COMPLETE_GI (curl -sL https://www.toptal.com/developers/gitignore/api/list)
     end
-    echo $__COMPLETE_GI | string split ","
+    # echo $__COMPLETE_GI | tr "," "\n" | tr " " "\n"
+    string split "," $__COMPLETE_GI | string trim
 end
 
 complete -c gi -f -a '(__complete_gi)'
