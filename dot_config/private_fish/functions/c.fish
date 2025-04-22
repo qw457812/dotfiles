abdf0a56701ea01e50a6bcf463508c47a0caf415
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-04-22 15:30:37 +0800

    fix(kitty): `cmd+enter` split and `c` clear in shell

--- dot_config/private_fish/functions/c.fish
+++ dot_config/private_fish/functions/c.fish
@@ -3,4 +3,8 @@ function c
     if set -q TMUX
         command tmux clear-history
     end
+    if set -q KITTY_PID
+        # clear-screen-and-scrollback | https://github.com/kovidgoyal/kitty/blob/c1a987353037837c1a80234ce3d07335d48931a8/kitty/options/definition.py#L4345-L4347
+        printf "\e[H\e[3J"
+    end
 end
