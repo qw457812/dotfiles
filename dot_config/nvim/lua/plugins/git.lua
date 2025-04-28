f6746fe9f2d99419346f53cef0805de66530f5c3
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-04-29 01:15:59 +0800

    fix(nvim-diffview): <bs> to close

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -215,39 +215,60 @@ return {
       { "<leader>gvd", "<cmd>DiffviewFileHistory<CR>", desc = "Diff Repo" },
       { "<leader>gvf", "<cmd>DiffviewFileHistory %<CR>", desc = "File History" },
     },
+    ---@param opts DiffviewConfig
     opts = function(_, opts)
       local actions = require("diffview.actions")
 
-      -- vim.api.nvim_create_autocmd({ "WinEnter", "BufEnter" }, {
-      --   group = vim.api.nvim_create_augroup("user_diffview", {}),
-      --   pattern = "diffview:///panels/*",
-      --   callback = function()
-      --     vim.opt_local.cursorline = true
-      --     vim.opt_local.winhighlight = "CursorLine:WildMenu"
-      --   end,
-      -- })
+      LazyVim.extend(opts, "keymaps.view", {
+        { "n", "q", actions.close, { desc = "Close" } },
+      })
+      LazyVim.extend(opts, "keymaps.file_panel", {
+        { "n", "q", actions.close, { desc = "Close" } },
+        {
+          "n",
+          "<Esc>",
+          function()
+            if not U.keymap.clear_ui_esc() then
+              actions.close()
+            end
+          end,
+          desc = "Clear UI or Close",
+        },
+        -- {
+        --   "n",
+        --   "l",
+        --   function()
+        --     actions.select_entry()
+        --     actions.close()
+        --   end,
+        --   { desc = "Open" },
+        -- },
+      })
+      LazyVim.extend(opts, "keymaps.file_history_panel", {
+        { "n", "q", "<cmd>DiffviewClose<CR>", { desc = "Close" } },
+      })
 
       return U.extend_tbl(opts, {
         enhanced_diff_hl = true,
         view = {
-          default = { winbar_info = true },
-          file_history = { winbar_info = true },
+          default = {
+            -- FIXME: dropbar
+            winbar_info = true,
+          },
+          merge_tool = {
+            layout = "diff3_mixed",
+          },
+          file_history = {
+            winbar_info = true,
+          },
         },
         hooks = {
-          diff_buf_read = function(bufnr)
-            vim.b[bufnr].view_activated = false
+          view_opened = function(view)
+            vim.t[view.tabpage].user_diffview = true
+            if view.class:name() == "DiffView" then
+              actions.toggle_files() -- Close DiffView:FilePanel initially
+            end
           end,
-        },
-        keymaps = {
-          view = {
-            { "n", "q", actions.close },
-          },
-          file_panel = {
-            { "n", "q", actions.close },
-          },
-          file_history_panel = {
-            { "n", "q", "<cmd>DiffviewClose<CR>" },
-          },
         },
       })
     end,
