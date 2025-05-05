87ba9b7f83752cf1244414edfe4fe44b932e049e
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-05-06 01:21:05 +0800

    fix(nvim): count and dot-repeat for `sdd`

--- dot_config/nvim/lua/plugins/extras/editor/mini-operators.lua
+++ dot_config/nvim/lua/plugins/extras/editor/mini-operators.lua
@@ -1,46 +1,3 @@
-local H = {}
-
--- copied from: https://github.com/chrisgrieser/.config/blob/9bc8b38e0e9282b6f55d0b6335f98e2bf9510a7c/nvim/lua/personal-plugins/misc.lua#L109
-function H.smartDuplicate()
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local line = vim.api.nvim_get_current_line()
-
-  -- FILETYPE-SPECIFIC TWEAKS
-  if vim.bo.ft == "css" then
-    local newLine = line
-    if line:find("top:") then
-      newLine = line:gsub("top:", "bottom:")
-    end
-    if line:find("bottom:") then
-      newLine = line:gsub("bottom:", "top:")
-    end
-    if line:find("right:") then
-      newLine = line:gsub("right:", "left:")
-    end
-    if line:find("left:") then
-      newLine = line:gsub("left:", "right:")
-    end
-    line = newLine
-  elseif vim.bo.ft == "javascript" or vim.bo.ft == "typescript" then
-    line = line:gsub("^(%s*)if(.+{)$", "%1} else if%2")
-  elseif vim.bo.ft == "lua" then
-    line = line:gsub("^(%s*)if( .* then)$", "%1elseif%2")
-  elseif vim.bo.ft == "zsh" or vim.bo.ft == "bash" then
-    line = line:gsub("^(%s*)if( .* then)$", "%1elif%2")
-  elseif vim.bo.ft == "python" then
-    line = line:gsub("^(%s*)if( .*:)$", "%1elif%2")
-  end
-
-  -- INSERT DUPLICATED LINE
-  vim.api.nvim_buf_set_lines(0, row, row, false, { line })
-
-  -- MOVE CURSOR DOWN, AND TO VALUE/FIELD (IF EXISTS)
-  local _, luadocFieldPos = line:find("%-%-%-@%w+ ")
-  local _, valuePos = line:find("[:=][:=]? ")
-  local targetCol = luadocFieldPos or valuePos or col
-  vim.api.nvim_win_set_cursor(0, { row + 1, targetCol })
-end
-
 return {
   {
     "echasnovski/mini.operators",
@@ -65,7 +22,15 @@ return {
       { "cX", "cx$", desc = "Exchange to EoL", remap = true }, -- `vim.opt.timeoutlen` has increased from 300 to 500 for `cX` since which-key v3
       { "X", mode = "x", desc = "Exchange Selection" },
       { "sd", mode = { "n", "x" }, desc = "Multiply Operator" },
-      { "sdd", H.smartDuplicate, desc = "Duplicate Line" },
+      {
+        "sdd",
+        function()
+          vim.o.operatorfunc = "v:lua.require'util.keymap'.smartDuplicate"
+          return "g@l"
+        end,
+        expr = true,
+        desc = "Duplicate Line",
+      },
       { "sD", "sd$", desc = "Multiply to EoL", remap = true },
       { "g=", mode = { "n", "x" }, desc = "Evaluate Operator" },
       { "so", mode = { "n", "x" }, desc = "Sort Operator" },
