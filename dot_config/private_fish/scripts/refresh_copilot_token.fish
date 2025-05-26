35a8059786692d5bbea0bb16b25820065c623045
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-05-26 19:57:06 +0800

    fix(aider): refresh copilot token every 25 minutes

---
+++ dot_config/private_fish/scripts/refresh_copilot_token.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+# added to crontab as this token expires in 30 minutes
+# */25 * * * * ~/.config/fish/scripts/refresh_copilot_token.fish >/dev/null 2>&1
+
+if test -f ~/.config/github-copilot/apps.json
+    # https://github.com/Aider-AI/aider/issues/2227#issuecomment-2877805565
+    # https://github.com/Aider-AI/aider/issues/2227#issuecomment-2884719517
+    set -Ux OPENAI_API_KEY (curl -s -H "Authorization: Bearer $(cat ~/.config/github-copilot/apps.json | jq -r 'to_entries[0].value.oauth_token')" "https://api.github.com/copilot_internal/v2/token" | jq -r '.token')
+end
