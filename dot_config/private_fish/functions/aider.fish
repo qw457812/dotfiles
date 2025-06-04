39b649c271c7f6ff99cb3df82a8335c10f5f0639
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-06-04 12:09:20 +0800

    chore(aider): up
    Aider 0.84.0 refreshes copilot token automatically.

--- dot_config/private_fish/functions/aider.fish
+++
@@ -1,4 +0,0 @@
-function aider --wraps=aider
-    ~/.config/fish/scripts/refresh_copilot_token.fish
-    command aider $argv
-end
--- dot_config/private_fish/scripts/executable_refresh_copilot_token.fish
+++ dot_config/private_fish/scripts/executable_refresh_copilot_token.fish
@@ -1,10 +1,11 @@
 #!/usr/bin/env fish
 
-# add to crontab as this token expires in 30 minutes (not working, removed from crontab)
+# add to crontab as this token expires in 30 minutes (not working and not needed for aider, removed from crontab)
 # */25 * * * * ~/.config/fish/scripts/refresh_copilot_token.fish >/dev/null 2>&1
 
 if test -f ~/.config/github-copilot/apps.json
     # https://github.com/Aider-AI/aider/issues/2227#issuecomment-2877805565
     # https://github.com/Aider-AI/aider/issues/2227#issuecomment-2884719517
+    # https://github.com/Aider-AI/aider/issues/2227#issuecomment-2933308760
     set -Ux OPENAI_API_KEY (curl -s -H "Authorization: Bearer $(cat ~/.config/github-copilot/apps.json | jq -r 'to_entries[0].value.oauth_token')" "https://api.github.com/copilot_internal/v2/token" | jq -r '.token')
 end
