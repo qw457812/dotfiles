f0ac50d25e8040ced5094168ee19cc5113745855
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-06-17 12:34:56 +0800

    fix(nvim-blink): adapt command-line window hack for latest blink.cmp changes

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -224,7 +224,10 @@ return {
             "snippet_forward",
             H.actions.copilot_nes,
             function(cmp)
-              if has_words_before() then
+              if
+                has_words_before()
+                or vim.fn.getcmdwintype() == ":" -- do not fallback to popup menu in command-line window
+              then
                 return cmp.show()
               end
             end,
@@ -369,31 +372,12 @@ return {
                   for _, item in ipairs(items) do
                     local text_edit = item.textEdit
                     if text_edit then
-                      -- see: https://github.com/saghen/blink.cmp/blob/f820d64680bb5679aa3f493a1b20a06128fc14e0/lua/blink/cmp/sources/cmdline/init.lua#L155-L168
+                      -- see: https://github.com/Saghen/blink.cmp/blob/9c5d82370cf2e5f28c27437f4a88d315ace1844f/lua/blink/cmp/sources/cmdline/init.lua#L232-L245
                       text_edit.insert.start.line = ctx.cursor[1] - 1
                       text_edit.insert["end"].line = ctx.cursor[1] - 1
                       text_edit.replace.start.line = ctx.cursor[1] - 1
                       text_edit.replace["end"].line = ctx.cursor[1] - 1
                       text_edit.insert["end"].character = text_edit.replace["end"].character
-
-                      -- see: https://github.com/saghen/blink.cmp/blob/f820d64680bb5679aa3f493a1b20a06128fc14e0/lua/blink/cmp/sources/cmdline/init.lua#L126-L136
-                      local is_lua_expr = ctx.line:sub(1, 1) == "="
-                      if is_lua_expr then
-                        -- copied from: https://github.com/saghen/blink.cmp/blob/f820d64680bb5679aa3f493a1b20a06128fc14e0/lua/blink/cmp/sources/cmdline/init.lua#L24-L32
-                        local arguments = vim.split(ctx.line, " ", { plain = true })
-                        local arg_number = #vim.split(ctx.line:sub(1, ctx.cursor[2]), " ", { plain = true })
-                        local text_before_argument = table.concat(
-                          require("blink.cmp.lib.utils").slice(arguments, 1, arg_number - 1),
-                          " "
-                        ) .. (arg_number > 1 and " " or "")
-
-                        local current_arg = arguments[arg_number]
-                        local keyword_config = require("blink.cmp.config").completion.keyword
-                        local keyword = ctx.get_bounds(keyword_config.range)
-                        local current_arg_prefix = current_arg:sub(1, keyword.start_col - #text_before_argument - 1)
-
-                        text_edit.newText = current_arg_prefix:sub(2, -1) .. text_edit.newText
-                      end
                     end
                   end
                 end
