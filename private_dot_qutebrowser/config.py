eac2336b141736106ad8cabcd9a0cedd9399d71f
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-07-02 14:41:11 +0800

    feat(qutebrowser): use vim.g.shell_command_editor for i_CTRL-E

--- private_dot_qutebrowser/config.py
+++ private_dot_qutebrowser/config.py
@@ -1,22 +1,36 @@
+import catppuccin
+
 c = c  # noqa: F821
 config = config  # noqa: F821
 
 config.load_autoconfig(False)
 
+# https://github.com/catppuccin/qutebrowser#manual-config
+catppuccin.setup(c, "frappe")  # mocha, macchiato, frappe
+
 # https://github.com/qutebrowser/qutebrowser/blob/main/doc/help/settings.asciidoc
 c.fonts.default_family = "Maple Mono NF CN"
 c.fonts.default_size = "12pt"
 
-c.tabs.position = "left"
 c.tabs.show = "multiple"
-c.tabs.width = "10%"
+c.tabs.title.format = "{audio}{current_title}"
+c.tabs.position = "left"
+c.tabs.width = 200
+c.tabs.padding = {"bottom": 7, "left": 5, "right": 0, "top": 7}
+c.tabs.indicator.width = 0
+c.tabs.last_close = "close"
+
+c.downloads.position = "bottom"
+c.downloads.location.directory = "~/Downloads"
+c.downloads.location.prompt = False
+
+c.window.hide_decoration = True
+c.colors.webpage.darkmode.enabled = True
 
 c.auto_save.session = True
-c.colors.webpage.darkmode.enabled = True
-c.window.hide_decoration = True
-c.downloads.position = "bottom"
+c.keyhint.delay = 300  # which key
 # c.scrolling.smooth = True
-
+c.url.searchengines = {"DEFAULT": "https://google.com/search?q={}"}
 c.editor.command = [
     "/opt/homebrew/bin/kitty",
     "-e",
@@ -25,4 +39,6 @@ c.editor.command = [
     "{file}",
     "-c",
     "normal {line}G{column0}l",
+    "--cmd",
+    "lua vim.g.shell_command_editor = true",
 ]
