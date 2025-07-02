qw457812/dotfiles

commit 3ac49f6a80fb8f7989ad913af2d9d57e0eb3128f

chore(nvim): up

dot_config/nvim/lua/util/init.lua
@@ -82,18 +82,11 @@ function M.is_floating_win(win, opts)
     local is_tsc = package.loaded["treesitter-context"]
       and (vim.w[win].treesitter_context or vim.w[win].treesitter_context_line_number)
 
-    local is_snacks_explorer = (function()
-      local picker = Snacks.picker.get({ source = "explorer" })[1]
-      if not picker then
-        return false
-      end
-      for _, w in pairs(picker.layout.wins or {}) do
-        if w.win == win then
-          return true
-        end
-      end
-      return false
-    end)()
+    local is_snacks_explorer = vim.iter(Snacks.picker.get({ source = "explorer" })):any(function(picker)
+      return vim.iter(picker.layout.wins or {}):any(function(w)
+        return w.win == win
+      end)
+    end)
 
     if is_tsc or is_snacks_explorer or vim.list_contains({ "snacks_dashboard", "layers_help" }, ft) then
       return false
@@ -124,14 +117,11 @@ function M.is_edgy_win(win)
   end
   win = win or 0
   win = win == 0 and vim.api.nvim_get_current_win() or win
-  for _, edgebar in pairs(require("edgy.config").layout) do
-    for _, w in ipairs(edgebar.wins) do
-      if win == w.win then
-        return true
-      end
-    end
-  end
-  return false
+  return vim.iter(require("edgy.config").layout):any(function(edgebar)
+    return vim.iter(edgebar.wins):any(function(w)
+      return w.win == win
+    end)
+  end)
 end
 
 ---@param mode? string

dot_config/nvim/lua/util/keymap.lua
@@ -135,8 +135,12 @@ function M.smart_duplicate_line()
     line = line:gsub("^(%s*)if( .* then)$", "%1elseif%2")
   elseif ft == "zsh" or ft == "bash" then
     line = line:gsub("^(%s*)if( .* then)$", "%1elif%2")
+  elseif ft == "fish" then
+    line = line:gsub("^(%s*)if( .*)$", "%1else if%2")
   elseif ft == "python" then
     line = line:gsub("^(%s*)if( .*:)$", "%1elif%2")
+  elseif ft == "java" or ft == "scala" then
+    line = line:gsub("^(%s*)if( %(.+%) %{)$", "%1} else if%2")
   end
 
   -- insert duplicated line

commit 3658461f1366bbc3c7c653b62e928933807dc5e5

fix(nvim-util): `U.is_floating_win()` and `U.is_edgy_win()` caused by 3ac49f6

dot_config/nvim/lua/util/init.lua
@@ -83,7 +83,7 @@ function M.is_floating_win(win, opts)
       and (vim.w[win].treesitter_context or vim.w[win].treesitter_context_line_number)
 
     local is_snacks_explorer = vim.iter(Snacks.picker.get({ source = "explorer" })):any(function(picker)
-      return vim.iter(picker.layout.wins or {}):any(function(w)
+      return vim.iter(picker.layout.wins or {}):any(function(_, w)
         return w.win == win
       end)
     end)
@@ -117,7 +117,7 @@ function M.is_edgy_win(win)
   end
   win = win or 0
   win = win == 0 and vim.api.nvim_get_current_win() or win
-  return vim.iter(require("edgy.config").layout):any(function(edgebar)
+  return vim.iter(require("edgy.config").layout):any(function(_, edgebar)
     return vim.iter(edgebar.wins):any(function(w)
       return w.win == win
     end)