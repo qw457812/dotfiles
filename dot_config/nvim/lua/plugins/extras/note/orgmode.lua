555df38066899ad25de6e785462da257092908a1
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-07-07 02:50:34 +0800

    feat(nvim-orgmode): format org files with `gq` on save/leader-cf

--- dot_config/nvim/lua/plugins/extras/note/orgmode.lua
+++ dot_config/nvim/lua/plugins/extras/note/orgmode.lua
@@ -20,5 +20,39 @@ return {
         prefix = "<localleader>",
       },
     },
+    init = function()
+      LazyVim.on_very_lazy(function()
+        LazyVim.format.register({
+          name = "orgmode",
+          priority = 0,
+          primary = true,
+          -- see:
+          -- - https://github.com/nvim-orgmode/orgmode/blob/7ffb34c622e1c64323e56fdb571b775db021fec1/docs/configuration.org?plain=1#L2341-L2359
+          -- - https://github.com/nvim-orgmode/orgmode/blob/27ab1cf9e7ae142f9e9ffb218be50dd920f04cb3/ftplugin/org.lua#L30
+          format = function(buf)
+            vim.api.nvim_buf_call(buf, function()
+              local mode = vim.api.nvim_get_mode().mode
+              if mode == "v" or mode == "V" then
+                vim.cmd("normal! gq")
+              else
+                vim.cmd("lockmarks normal! zRmzgggqG`z")
+                vim.cmd("delmarks z")
+              end
+            end)
+          end,
+          sources = function(buf)
+            return vim.bo[buf].filetype == "org" and { "orgmode" } or {}
+          end,
+        })
+      end)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "org",
+        callback = function(ev)
+          -- vim.b[ev.buf].autoformat = false
+          vim.bo[ev.buf].textwidth = 999 -- disable textwidth for gq, related to https://github.com/nvim-orgmode/orgmode/issues/144#issuecomment-965049864
+        end,
+      })
+    end,
   },
 }
