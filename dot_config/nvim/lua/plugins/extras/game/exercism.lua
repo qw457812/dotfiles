de1ead3b9a4d6231cb96724e99316cb66c283bff
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-07-24 00:47:11 +0800

    chore(nvim-exercism): up

--- dot_config/nvim/lua/plugins/extras/game/exercism.lua
+++ dot_config/nvim/lua/plugins/extras/game/exercism.lua
@@ -12,16 +12,17 @@ return {
     "2kabhishek/exercism.nvim",
     dependencies = {
       "2kabhishek/utils.nvim",
-      -- "2kabhishek/termim.nvim", -- optional, better UI for ExercismTest
+      -- "2kabhishek/termim.nvim", -- optional, better UI for `Exercism test`
     },
-    cmd = { "ExercismLanguages", "ExercismList", "ExercismSubmit", "ExercismTest" },
+    cmd = "Exercism",
     keys = {
-      { "<leader>Ea", "<cmd>ExercismLanguages<cr>", desc = "Exercism Languages" },
-      { "<leader>El", "<cmd>ExercismList<cr>", desc = "Exercism List" },
-      { "<leader>Es", "<cmd>ExercismSubmit<cr>", desc = "Exercism Submit" },
-      { "<leader>Et", "<cmd>ExercismTest<cr>", desc = "Exercism Test" },
+      { "<leader>Ea", "<cmd>Exercism languages<cr>", desc = "Languages" },
+      { "<leader>El", "<cmd>Exercism list<cr>", desc = "List" },
+      { "<leader>Es", "<cmd>Exercism submit<cr>", desc = "Submit" },
+      { "<leader>Et", "<cmd>Exercism test<cr>", desc = "Test" },
     },
     opts = {
+      use_new_command = true,
       add_default_keybindings = false,
       exercism_workspace = exercism_workspace(),
       default_language = "python",
