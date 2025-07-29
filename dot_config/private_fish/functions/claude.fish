492aa5ce8a0a4c43b93812033d2cce28ed831b51
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-07-29 22:34:02 +0800

    chore: up

--- dot_config/private_fish/functions/claude.fish
+++ dot_config/private_fish/functions/claude.fish
@@ -1,6 +1,8 @@
 # @fish-lsp-disable 4004
 function claude --wraps=claude
-    set -lx ANTHROPIC_BASE_URL https://tokhub.ai
-    set -lx ANTHROPIC_API_KEY $TOKHUB_API_KEY
-    command claude $argv
+    # set -lx ANTHROPIC_BASE_URL https://tokhub.ai
+    # set -lx ANTHROPIC_API_KEY $TOKHUB_API_KEY
+    set -lx ANTHROPIC_BASE_URL https://claude.ctok.ai/api/ # https://us.ctok.ai/api/
+    set -lx ANTHROPIC_AUTH_TOKEN $CTOK_AUTH_TOKEN
+    command claude --verbose $argv
 end
