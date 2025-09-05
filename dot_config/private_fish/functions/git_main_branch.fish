73289458e3db57d55c27a4a0f456d2fff6682201
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-09-05 19:21:50 +0800

    chore(fish): update git_main_branch function to follow ohmyzsh changes
    See: https://github.com/ohmyzsh/ohmyzsh/commit/2525dae6613652ec9cb572bdc2fdf80ef837a967

--- dot_config/private_fish/functions/git_main_branch.fish
+++ dot_config/private_fish/functions/git_main_branch.fish
@@ -1,11 +1,22 @@
-# copied from: https://github.com/ohmyzsh/ohmyzsh/blob/750d3ac4b493dca13ef0ced55fa6a2cd02dc7ee8/plugins/git/git.plugin.zsh#L35-L48
-# Check if main exists and use instead of master
+# copied from: https://github.com/ohmyzsh/ohmyzsh/blob/b9c9fcfd3fb93c43b456ddb608308c9ac9bffab1/plugins/git/git.plugin.zsh#L35-L58
+# Get the default branch name from common branch names or fallback to remote HEAD
 function git_main_branch
     command git rev-parse --git-dir &>/dev/null || return
-    set -l ref
+
+    set -l remote ref
+
     for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk,mainline,default,stable,master}
         if command git show-ref -q --verify $ref
             echo (basename $ref)
+            return 0
+        end
+    end
+
+    # Fallback: try to get the default branch from remote HEAD symbolic refs
+    for remote in origin upstream
+        set ref (command git rev-parse --abbrev-ref $remote/HEAD 2>/dev/null)
+        if string match -q "$remote/*" $ref
+            echo (string replace "$remote/" "" $ref)
             return 0
         end
     end
