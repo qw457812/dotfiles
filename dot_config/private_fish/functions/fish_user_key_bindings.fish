ab5748455d34e1b9130738dec599690fa17a51eb
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-09-29 10:37:26 +0800

    feat(fish): vi keybindings and termux clipboard support

--- dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -4,6 +4,7 @@ function __edit_command_buffer
     edit_command_buffer
 end
 
+# see `bind -p` for default bindings
 function fish_user_key_bindings
     set -g fish_key_bindings fish_vi_key_bindings
     fish_default_key_bindings -M insert
@@ -11,8 +12,18 @@ function fish_user_key_bindings
 
     bind -M visual -m default y 'fish_clipboard_copy; commandline -f end-selection repaint-mode'
     bind yy fish_clipboard_copy
-    bind p 'set -g fish_cursor_end_mode exclusive' forward-char 'set -g fish_cursor_end_mode inclusive' fish_clipboard_paste
-    bind P fish_clipboard_paste
+    if set -q TERMUX_VERSION
+        # # https://github.com/fish-shell/fish-shell/issues/10807#issuecomment-2427887477
+        # bind p 'set -g fish_cursor_end_mode exclusive' forward-char 'set -g fish_cursor_end_mode inclusive' 'commandline -i -- (termux-clipboard-get)'
+        # bind P 'commandline -i -- (termux-clipboard-get)'
+        #
+        # https://github.com/fish-shell/fish-shell/blob/6f0532460a5be9c06fb15caa1f00e716ecef6420/share/functions/fish_clipboard_paste.fish#L26
+        bind p 'set -g fish_cursor_end_mode exclusive' forward-char 'set -g fish_cursor_end_mode inclusive' '__fish_paste (termux-clipboard-get)'
+        bind P '__fish_paste (termux-clipboard-get)'
+    else
+        bind p 'set -g fish_cursor_end_mode exclusive' forward-char 'set -g fish_cursor_end_mode inclusive' fish_clipboard_paste
+        bind P fish_clipboard_paste
+    end
 
     bind -M visual -m default v '__edit_command_buffer; commandline -f end-selection repaint-mode'
     bind -m visual V beginning-of-line begin-selection end-of-line repaint-mode
@@ -33,5 +44,17 @@ function fish_user_key_bindings
     bind y,L kill-line yank
     bind y,H backward-kill-line yank
 
-    # TODO: https://github.com/fish-shell/fish-shell/issues/10807#issuecomment-2427887477
+    # readline style bindings
+    bind alt-k kill-line
+    # ctrl-k was hijacked by tmux/kitty/wezterm for window navigation
+    bind -M insert alt-k kill-line
+    bind -M visual alt-k kill-line
+
+    # ctrl-l was hijacked by tmux/kitty/wezterm for window navigation
+    # bind alt-l clear-screen
+    # bind -M insert alt-l clear-screen
+    # bind -M visual alt-l clear-screen
+    bind alt-l c repaint-mode
+    bind -M insert alt-l c repaint-mode
+    bind -M visual alt-l c repaint-mode
 end
