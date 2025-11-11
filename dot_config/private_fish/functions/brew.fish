648a68b2645742a7ca790c331b5986b6acf38028
qw457812 <37494864+qw457812@users.noreply.github.com>
2025-11-11 13:22:44 +0800

    fix(fish): unknown command brew on termux

--- dot_config/private_fish/functions/brew.fish
+++ dot_config/private_fish/functions/brew.fish
@@ -1,7 +1,9 @@
+type -q brew; or return
+
 function brew --wraps=brew
     command brew $argv
 
     if string match -q -r "upgrade|update|outdated" -- $argv
-        sketchybar --trigger brew_update
+        type -q sketchybar; and sketchybar --trigger brew_update
     end
 end
