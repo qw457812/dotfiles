f00da3f2a7e12f0b93770825307a4ac5fe0ce434
qw457812 <37494864+qw457812@users.noreply.github.com>
2026-09-08 19:22:25 +0800

    refactor(nvim/typescript): adopt LazyVim-native tsc LSP support
    TS 7 renamed the tsgo LSP to tsc, and LazyVim c8cd5a0c (#7233) now
    registers tsc as a valid vim.g.lazyvim_ts_lsp value and ships a tsc
    extra that disables tsgo and configures tsc with the same filetypes
    and inlay hints our manual server swap replicated. Drop the redundant
    override in typescript.lua, set tsc directly in options.lua, and bump
    LazyVim in the macOS lock file.

--- dot_config/nvim/lua/plugins/lang/typescript.lua
+++ dot_config/nvim/lua/plugins/lang/typescript.lua
@@ -25,46 +25,4 @@ return {
       end
     end,
   },
-
-  -- TypeScript 7 renamed the `tsgo` lsp to `tsc` (nvim-lspconfig #4493).
-  -- LazyVim only accepts `tsgo`/`vtsls` for `vim.g.lazyvim_ts_lsp`, so keep that global
-  -- as-is and swap the servers here until LazyVim registers `tsc` itself.
-  {
-    "neovim/nvim-lspconfig",
-    ---@param opts PluginLspOpts
-    opts = function(_, opts)
-      if vim.g.lazyvim_ts_lsp ~= "tsgo" then
-        return
-      end
-
-      return U.extend_tbl(opts, {
-        ---@type table<string, lazyvim.lsp.Config|boolean>
-        servers = {
-          tsgo = {
-            enabled = false,
-          },
-          -- https://github.com/LazyVim/LazyVim/blob/d07070bf2ff83ae513097d02d71460920af85a91/lua/lazyvim/plugins/extras/lang/typescript/tsgo.lua#L24-L48
-          -- https://github.com/neovim/nvim-lspconfig/blob/3fc5c454b5a903049c8096b34e60ed30d1891aae/lsp/tsc.lua#L71-L104
-          tsc = {
-            filetypes = {
-              "javascript",
-              "javascriptreact",
-              "javascript.jsx",
-              "typescript",
-              "typescriptreact",
-              "typescript.tsx",
-            },
-            settings = {
-              ["js/ts"] = {
-                inlayHints = {
-                  functionLikeReturnTypes = { enabled = false },
-                  variableTypes = { enabled = false },
-                },
-              },
-            },
-          },
-        },
-      } --[[@as PluginLspOpts]])
-    end,
-  },
 }
